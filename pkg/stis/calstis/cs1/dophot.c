39d47f7b9a945fce82fe7ad6198db0f7beb09d21
hodge <hodge@stsci.edu>
2011-11-07 21:39:09 +0000

    A new function removeA2D has been added, and it is called in doPhot in order to search for "A2D" in the photmode string.  If found, that component will be removed before calling GetPhotMode, and the photflam that GetPhotMode returns will be explicitly multiplied by the atodgain that was previously read from the CCDTAB.  See Trac #796.
    git-svn-id: http://svn.stsci.edu/svn/ssb/stsci_python/hstcal/trunk@13972 fe389314-cf27-0410-b35b-8c050e845b92

--- pkg/stis/calstis/cs1/dophot.c
+++ pkg/stis/calstis/cs1/dophot.c
@@ -11,6 +11,7 @@
 # include "stisdef.h"
 
 static void Phot2Obs (char *, char *);
+static int removeA2D (char *, char *);
 
 /* This routine reads the photmode string from the primary header (which
    must therefore have already been updated) and calls a function that reads
@@ -37,6 +38,12 @@ static void Phot2Obs (char *, char *);
 	Set photcorr to IGNORED (which results in PHOTCORR = "SKIPPED" in
 	the output header) if photmode not found or parameters are garbage.
 	Also set photcorr to IGNORED if the parameters are -9999.
+
+   Phil Hodge, 2011 Nov 7:
+	Add function removeA2D to look for "A2D" in the photmode string
+	and, if found, remove it when copying to a temporary photmode.
+	If "A2D" was found in photmode, multiply photflam by atodgain,
+	rather than letting the imphttab handle this factor.
 */
 
 int doPhot (StisInfo1 *sts, SingleGroup *x) {
@@ -51,15 +58,18 @@ SingleGroup *x    io: image to be calibrated; primary header is modified
 	PhotPar obs;
 
 	char photmode[STIS_LINE+1];	/* the photmode string */
+	char tempphot[STIS_LINE+1];	/* photmode without A2D */
 	char obsmode[STIS_LINE+1];	/* based on the photmode string */
 	int use_default = 1;	/* use default value if keyword is missing */
+	int found_a2d = 0;	/* true if there's a CCD A2D component */
 
 	/* Get PHOTMODE from the primary header. */
 	if ((status = Get_KeyS (x->globalhdr, "PHOTMODE",
 		use_default, "", photmode, STIS_LINE)) != 0)
 	    return (status);
 
-	Phot2Obs (photmode, obsmode);
+	found_a2d = removeA2D (photmode, tempphot);
+	Phot2Obs (tempphot, obsmode);
 
 	/* Initialize PhotPar struct. */
 	InitPhotPar (&obs, sts->phot.name, sts->phot.pedigree);
@@ -98,6 +108,8 @@ SingleGroup *x    io: image to be calibrated; primary header is modified
 
 	/* Update the photometry keyword values in the primary header. */
 
+	if (found_a2d)
+	    obs.photflam *= sts->atodgain;
 	if ((status = Put_KeyF (x->globalhdr, "PHOTFLAM", obs.photflam,
 			"inverse sensitivity")) != 0)
 	    return (status);
@@ -119,19 +131,72 @@ SingleGroup *x    io: image to be calibrated; primary header is modified
 	return (0);
 }
 
-/* This function converts the PHOTMODE string into an OBSMODE
-   string suitable for use with synphot functions.
-   PHOTMODE - all upper case component names separated by blanks
-   OBSMODE - all lower case names separated by commas
+/* This function looks for "A2D" in the photmode string and, if present,
+   removes it.
+*/
+
+static int removeA2D (char *photmode, char *tempphot) {
+
+	int i, j, k, n, len;
+	int foundit;		/* found "A2D"? */
+	int found_space;	/* found a space following "A2D<digit>"? */
+
+	len = strlen(photmode);
+
+	j = len;
+	k = len;
+	foundit = 0;
+	for (n = 0;  n < len-2;  n++) {
+	    if (toupper(photmode[n])   == 'A' &&
+		photmode[n+1]          == '2' &&
+		toupper(photmode[n+2]) == 'D') {
+		foundit = 1;
+		/* look for a space following "A2D" and digit(s) */
+		found_space = 0;
+		for (k = n + 3;  k < len;  k++) {
+		    if (photmode[k] == ' ') {
+			found_space = 1;
+			break;
+		    }
+		}
+		if (n == 0) {
+		    j = 0;
+		    k++;		/* start copying past the space */
+		} else if (found_space) {
+		    j = n - 1;		/* skip the previous space */
+		} else {
+		    j = n - 1;
+		}
+	   }
+	}
+
+	i = 0;
+	for (n = 0;  n < j;  n++) {
+	    tempphot[i] = photmode[n];
+	    i++;
+	}
+	for (n = k;  n < len;  n++) {
+	    tempphot[i] = photmode[n];
+	    i++;
+	}
+	tempphot[i] = '\0';
+
+	return foundit;
+}
+
+/* This function converts the photmode string into an obsmode string
+   suitable for use with the IMPHTTAB.
+   photmode - all upper case component names separated by blanks
+   obsmode - all lower case names separated by commas
 */
 static void Phot2Obs (char *photmode, char *obsmode) {
 
-	char blank = 32, comma = 44;
+	char blank = ' ', comma = ',';
 	int i, len, c1;
 
 	len = strlen(photmode);
 
-	for (i=0; i < len; i++) {
+	for (i=0;  i < len;  i++) {
 	    c1 = photmode[i];
 	    if (c1 == blank) {
 		obsmode[i] = comma;
