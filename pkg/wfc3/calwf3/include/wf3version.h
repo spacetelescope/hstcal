b3a37a1b712f87ee2e62b35d20d9985727f55e7c
iraf <iraf@stsci.edu>
2012-01-27 01:31:08 +0000

    automatic irafx update
    git-svn-id: http://svn.stsci.edu/svn/ssb/stsci_python/hstcal/branches/irafx@14523 fe389314-cf27-0410-b35b-8c050e845b92

---
+++ pkg/wfc3/calwf3/include/wf3version.h
@@ -0,0 +1,5 @@
+/* This string is written to the output primary header as CAL_VER. */
+define WF3_CAL_VER "2.6.1 (24-Jan-2012)"
+
+# define WF3_CAL_VER "3.0 (12-Sep-2011)"
+# define WF3_CAL_VER_NUM "3.0"
