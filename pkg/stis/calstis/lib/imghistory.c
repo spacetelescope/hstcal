a3c07f9958330562d6a94a68a5db12af52abdc70
iraf <iraf@stsci.edu>
2012-01-27 18:08:49 +0000

    automatic irafx update
    git-svn-id: http://svn.stsci.edu/svn/ssb/stsci_python/hstcal/branches/irafx@14608 fe389314-cf27-0410-b35b-8c050e845b92

---
+++ pkg/stis/calstis/lib/imghistory.c
@@ -0,0 +1,46 @@
+# include <stdio.h>
+# include <string.h>
+# include "c_iraf.h"
+# include "hstio.h"
+
+# include "stis.h"
+# include "stiserr.h"
+# include "stisdef.h"
+
+/* This routine writes history records for a reference image, including
+   the name of the file and the pedigree and descrip values.
+*/
+
+int ImgHistory (RefImage *ref, Hdr *phdr) {
+
+/* arguments:
+RefImage *ref     i: info about reference image
+Hdr *phdr         io: header to receive history records
+*/
+
+	char history[STIS_LINE];
+
+	strcpy (history, "  reference image ");
+	strcat (history, ref->name);
+	addHistoryKw (phdr, history);
+	if (hstio_err())
+	    return (HEADER_PROBLEM);
+
+	if (ref->pedigree[0] != '\0') {
+	    strcpy (history, "    ");
+	    strcat (history, ref->pedigree);
+	    addHistoryKw (phdr, history);
+	    if (hstio_err())
+		return (HEADER_PROBLEM);
+	}
+
+	if (ref->descrip[0] != '\0') {
+	    strcpy (history, "    ");
+	    strcat (history, ref->descrip);
+	    addHistoryKw (phdr, history);
+	    if (hstio_err())
+		return (HEADER_PROBLEM);
+	}
+
+	return (0);
+}
