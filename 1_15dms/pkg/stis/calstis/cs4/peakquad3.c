4d899cbb87764a72fa06199380902dd5813b249a
iraf <iraf@stsci.edu>
2012-04-16 19:40:24 +0000

    tagging opus 2012_1 delivery 15
    git-svn-id: http://svn.stsci.edu/svn/ssb/stsci_python/hstcal/tags/opus_2012@16255 fe389314-cf27-0410-b35b-8c050e845b92

---
+++ 1_15dms/pkg/stis/calstis/cs4/peakquad3.c
@@ -0,0 +1,26 @@
+# include <stdio.h>
+# include "stiserr.h"
+
+/* This routine finds the peak of the quadratic that passes through
+   three uniformly spaced points.  The peak can be either a maximum
+   or a minimum.
+*/
+
+int PeakQuad3 (double *y, double *x) {
+
+/* arguments:
+double *y      i: array of three points
+double *x      o: location of axis relative to middle point,
+		in units of the spacing of the independent variable
+*/
+
+	double denominator;
+
+	denominator = y[0] - 2. * y[1] + y[2];
+	if (denominator == 0.)
+	    return (NO_GOOD_DATA);
+
+	*x = (y[0] - y[2]) / (2. * denominator);
+
+	return (0);
+}
