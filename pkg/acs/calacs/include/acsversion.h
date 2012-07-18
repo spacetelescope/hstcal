38cc61abeb389bf30bea1789489ecf4211d86182
lim <lim@stsci.edu>
2012-07-18 18:26:39 +0000

    moved FLSHCORR to after PCTECORR as per #920
    git-svn-id: http://svn.stsci.edu/svn/ssb/stsci_python/hstcal/trunk@17927 fe389314-cf27-0410-b35b-8c050e845b92

--- pkg/acs/calacs/include/acsversion.h
+++ pkg/acs/calacs/include/acsversion.h
@@ -1,8 +1,8 @@
 /* This string is written to the output primary header as CAL_VER. */
 
 
-#define ACS_CAL_VER "8.0.5 (11-Jun-2012)"
-#define ACS_CAL_VER_NUM "8.0.5"
+#define ACS_CAL_VER "8.0.6 (18-Jul-2012)"
+#define ACS_CAL_VER_NUM "8.0.6"
 
 
 /* name and version number of the CTE correction algorithm */
