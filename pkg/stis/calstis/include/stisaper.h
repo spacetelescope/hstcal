de0f33e52ff902dca59f9fd489ae7e27a42145f8
iraf <iraf@stsci.edu>
2012-08-03 00:32:17 +0000

    delete for automatic irafx update
    git-svn-id: http://svn.stsci.edu/svn/ssb/stsci_python/hstcal/branches/irafx@18165 fe389314-cf27-0410-b35b-8c050e845b92

--- pkg/stis/calstis/include/stisaper.h
+++
@@ -1,5 +0,0 @@
-/* These are the suffixes for the current "pseudo-apertures."
-   "NULL" terminates the list.
-   This file is included only by lib/pseudoap.c.
-*/
-static char pseudo_ap[][STIS_CBUF+1] = {"E1", "E2", "D1", "NULL"};
