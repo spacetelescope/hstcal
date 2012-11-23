84e650ced20c9d1fc656cefc2ba1fa5c5ed9da83
iraf <iraf@stsci.edu>
2012-11-23 01:31:55 +0000

    delete for automatic irafx update
    git-svn-id: http://svn.stsci.edu/svn/ssb/stsci_python/hstcal/branches/irafx@20487 fe389314-cf27-0410-b35b-8c050e845b92

--- pkg/stis/calstis/include/stisvoltages.h
+++
@@ -1,5 +0,0 @@
-# define MIN_OCBABAV   (26.7024 + 22.0109) / 2.     /* CEB A&B amp bias */
-# define MIN_OCBCDAV   (26.7024 + 22.0388) / 2.     /* CEB C&D amp bias */
-# define MIN_OCBLGCDV  (-3.38383 - 6.11819) / 2.    /* CEB last gate C&D */
-# define MIN_OCBSWALV  (-6.0 - 7.0) / 2.            /* the summing wells */
-# define MIN_OCBRCDLV  (0.047692 + 0.0243553) / 2.  /* CEB reset gate CD Lo */
