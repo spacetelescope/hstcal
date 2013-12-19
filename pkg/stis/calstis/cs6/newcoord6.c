5ce700e942aaa98e493d3a0a3f35a77026d3cbb0
iraf <iraf@stsci.edu>
2013-12-19 18:48:45 +0000

    automatic ssbx update
    git-svn-id: http://svn.stsci.edu/svn/ssb/stsci_python/hstcal/branches/irafx@28061 fe389314-cf27-0410-b35b-8c050e845b92

---
+++ pkg/stis/calstis/cs6/newcoord6.c
@@ -0,0 +1,251 @@
+/* This file contains:
+	NewCoord6	add a CoordInfo record to the list
+	ReturnCoord6	extract a CoordInfo record from the list
+	RangeCoord6	find the range of sporders in the list
+	FreeCoord6	free memory
+	DebugCoord6	print debugging info
+internal:
+	CopyCoord	copy out one CoordInfo
+
+
+
+
+   Revision history:
+   ----------------
+   14 Nov 97  -  Borrowed from calstis7 (I.Busko)
+   14 Nov 97  -  Rename routines to avoid conflicts with cs7 (IB)
+   07 Jan 00  -  Add npix handling (IB)
+*/
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+
+# include "xtables.h"
+# include "stis.h"
+# include "calstis6.h"
+# include "stiserr.h"
+
+static void CopyCoord (CoordInfo *, CoordInfo *);
+
+/* This routine adds a CoordInfo record to the list.  The list is sorted
+   by sporder.  Before calling NewCoord6 for the first record to be inserted
+   in the list, *coords should have been initialized to NULL.
+*/
+
+int NewCoord6 (CoordInfo **coords, CoordInfo *input) {
+
+/* arguments:
+CoordInfo **coords  io: identifies the first record in the list
+CoordInfo *input    i: a new record to be inserted into the list
+*/
+
+	CoordInfo *current, *next, *newrec;
+	int done;
+
+	done = 0;
+
+	/* Allocate space for the new record, and copy to memory. */
+	if ((newrec = (CoordInfo *) malloc (sizeof (CoordInfo))) == NULL) {
+	    printf ("ERROR    Can't allocate memory in NewCoord.\n");
+	    return (OUT_OF_MEMORY);
+	}
+
+	CopyCoord (input, newrec);
+
+	/* Find where this record belongs in the list, sorting by sporder,
+	   and insert the record.
+	*/
+	current = *coords;
+
+	if ((*coords) == NULL) {
+	    /* The list is empty; newrec becomes the first record. */
+	    *coords = newrec;
+	    done = 1;
+	} else if (newrec->sporder <= (*coords)->sporder) {
+	    /* newrec is before the beginning of the list;
+		newrec becomes the first.
+	    */
+	    newrec->next = *coords;
+	    *coords = newrec;
+	    done = 1;
+	}
+
+	while (!done) {
+
+	    if (current->next == NULL) {
+		current->next = newrec;		/* add to end of list */
+		done = 1;
+
+	    } else {
+
+		next = current->next;
+
+		if (newrec->sporder <= next->sporder) {
+		    /* insert here */
+		    newrec->next = current->next;
+		    current->next = newrec;
+		    done = 1;
+		}
+	    }
+	    if (!done) {
+		current = next;
+	    }
+	}
+
+	return (0);
+}
+
+/* This routine returns a CoordInfo record from the list.  The record is
+   selected by sporder.
+
+   If *output is NULL, memory will be allocated for that record; otherwise,
+   it is assumed that *output is actually a valid pointer to a CoordInfo,
+   i.e. that the memory has been allocated.
+*/
+
+int ReturnCoord6 (CoordInfo **coords, int sporder, CoordInfo **output) {
+
+/* arguments:
+CoordInfo **coords  i: identifies the first record in the list
+CoordInfo **output   o: the record that matches sporder
+*/
+
+	CoordInfo *current;
+	int foundit = 0;
+
+	current = *coords;
+
+	/* Allocate space for the output. */
+	if (*output == NULL) {
+	    if ((*output = malloc (sizeof (CoordInfo))) == NULL) {
+		printf ("ERROR    Can't allocate memory in ReturnCoord.\n");
+		return (OUT_OF_MEMORY);
+	    }
+	    (*output)->next = NULL;
+	}
+
+	while (current != NULL) {
+	    if (current->sporder == sporder) {
+		foundit = 1;
+		break;
+	    }
+	    current = current->next;
+	}
+	if (foundit) 
+	    CopyCoord (current, *output);
+	else
+	    return (INVALID);	/* sporder not found, or empty list */
+
+	return (0);
+}
+
+
+/* This routine finds the minimum and maximum values of sporder in the
+   CoordInfo list.  The list is also checked to ensure that the sporder
+   numbers are consecutive and there are no duplicates.
+*/
+
+int RangeCoord6 (CoordInfo **coords, int *minorder, int *maxorder) {
+
+/* arguments:
+CoordInfo **coords        i: identifies the first record in the list
+int *minorder, *maxorder  o: minimum and maximum values of sporder
+*/
+
+	CoordInfo *current, *previous;
+
+	current = *coords;
+	if (current == NULL)
+	    return (-1);		/* empty list */
+
+	*minorder = current->sporder;		/* initial values */
+	*maxorder = current->sporder;
+
+	previous = current;
+	current = current->next;
+
+	while (current != NULL) {
+
+	    if (current->sporder != previous->sporder + 1) {
+		if (current->sporder == previous->sporder) {
+		    printf (
+		"ERROR    Duplicate order number %d in SDC table.\n",
+			current->sporder);
+		    return (1212);
+		} else {
+		    printf (
+		"ERROR    Order numbers in SDC table jump from %d to %d.\n",
+		    previous->sporder, current->sporder);
+		    return (1214);
+		}
+	    }
+	    *maxorder = current->sporder;
+	    previous = current;
+	    current = current->next;
+	}
+
+	return (0);
+}
+
+/* This routine frees all memory for the CoordInfo list.  The value of
+   coords is not changed.
+*/
+
+void FreeCoord6 (CoordInfo **coords) {
+
+/* argument:
+CoordInfo **coords  io: identifies the first record in the list
+*/
+
+	CoordInfo *current, *next;
+
+	current = *coords;
+
+	while (current != NULL) {
+	    next = current->next;
+	    free (current);
+	    current = next;
+	}
+
+	*coords = NULL;
+}
+
+/* This routine copies the current CoordInfo to output, which must
+   actually point to allocated space.  Note that in output, the pointer
+   to the next record is set to NULL.
+*/
+
+static void CopyCoord (CoordInfo *current, CoordInfo *output) {
+
+/* argument:
+CoordInfo *current  i: the current CoordInfo record
+CoordInfo *output   o: receives a copy of current
+*/
+
+	output->sporder  = current->sporder;
+	output->a2center = current->a2center;
+	output->cdelt2   = current->cdelt2;
+	output->npix     = current->npix;
+	output->next     = NULL;
+}
+
+/* This routine prints the spectral order number from the CoordInfo list. */
+
+void DebugCoord6 (CoordInfo **coords) {
+
+/* argument:
+CoordInfo **coords  i: identifies the first record in the list
+*/
+
+	CoordInfo *current;
+	int i = 1;
+
+	current = *coords;
+
+	while (current != NULL) {
+	    printf ("%3d:  %3d\n", i, current->sporder);
+	    current = current->next;
+	    i++;
+	}
+}
---
+++ pkg/wfc3/calwf3/wf3rej/cr_scaling.c
@@ -0,0 +1,198 @@
+# include   <stdio.h>
+# include   <string.h>
+# include "hstio.h"
+
+# include   "wf3.h"
+# include   "wf3err.h"
+# include   "wf3info.h"
+# include   "rej.h"
+
+/*  cr_scaling -- Determine the scaling factors according to exposure times or
+        other user specified scheme. */
+
+/*
+Description:
+------------
+If using the exposure time, the scaling factors are normalized to ratios 
+relative to the max exposure. 
+
+    Date            Author      Description
+    ----            ------      -----------
+    24-Sep-1998     W.J. Hack   Initial Version
+    18-Mar-1999     W.J. Hack   Revised to read EXPTIMEs from Primary headers
+                                using image-template list directly
+    20-Oct-1999     W.J. Hack   Revised to compute number of good input images
+                                and insure they are less than MAX_FILES.
+    14-Apr-2000     W.J. Hack   Revised to also return final EXPEND appropriate
+                                for output CR-combined product
+    18-Jun-2002     H. Bushouse Modified to fine initial EXPSTART and return it
+				(following CALACS changes).
+    27-Oct-2003     H. Bushouse Upgrades for handling inputs with EXPTIME=0
+				(following CALACS changes).
+    14-Dec-2011     H. Bushouse Upgraded to read BUNIT keyword value from each
+				input image. (PR 69969; Trac #814)
+    07-May-2012 M. Sosey, updated the BUNIT read to be case insensitive
+                (trac #887)
+*/
+
+int cr_scaling (char *expname, IRAFPointer tpin, float efac[], int *nimgs,
+		double *expend, double *expstart, DataUnits bunit[]) {
+
+    extern int status;
+
+    Hdr         hdr;
+    int         nzero, k;
+    char        fdata[SZ_FNAME + 1], units[12];
+    IODescPtr   ip;
+    int         numimgs;        /* How many good input images are there? */
+
+    double      end, keyend, keystart, start;
+
+    int         GetKeyFlt (Hdr *, char *, int, float, float *);
+    int         GetKeyDbl (Hdr *, char *, int, double, double *);
+    int         streq_ic (char *, char *);/* strings equal? (case insensitive)*/
+
+    /* -------------------------------- begin ------------------------------- */
+
+    /* Rewind the image template pointer */
+    c_imtrew(tpin);
+
+    *nimgs = c_imtlen(tpin);
+    end = 0.0;
+    keyend = 0.0;
+    start = 1e+10;
+    keystart = 0.0;
+ 
+    /* Check to make sure there are not too many images to work with... */
+    if (*nimgs > MAX_FILES) {
+        trlerror("There are too many input images to combine. "); 
+        return(status = NOTHING_TO_DO);
+    }
+
+    /* if the parameter scaling is null, all images have equal weight. 
+        If no keyword name is given for the exposure time, assume equal
+        weights of 1 for all images.
+    */
+    if (expname[0] == '\0') {
+        return (status);
+    }
+
+    /* Use exposure time as scaling factor */
+    nzero = 0;	 
+ 
+    /* loop all input files counting how many usable inputs there are */
+    numimgs = 0;
+    for (k = 0; k < *nimgs; ++k) {
+
+        /* read the next input image name in the template list */
+        c_imtgetim (tpin, fdata, SZ_FNAME);
+
+        /* open the primary header */
+        ip = openInputImage (fdata, "", 0);
+        if (hstio_err()) {
+            sprintf (MsgText, "Cannot open data file '%s'", fdata);
+            trlerror (MsgText);
+            return (status = OPEN_FAILED);
+        }
+
+        initHdr (&hdr);
+
+        /* read in primary header from image */
+        getHeader (ip, &hdr);
+
+        if (GetKeyFlt (&hdr, expname, USE_DEFAULT, 0., &efac[k]) != 0) {
+            sprintf (MsgText,
+		     "cannot read '%s' from the primary header of '%s'",
+		      expname, fdata);
+            trlerror (MsgText);
+            freeHdr (&hdr);
+            return(status = KEYWORD_MISSING);
+        }
+        
+        if (efac[k] < 0.) {
+            sprintf (MsgText, "exposure time of file '%s' is negative", fdata);
+            trlerror (MsgText);
+            freeHdr (&hdr);
+            return(status = INVALID_VALUE);
+        }
+        if (efac[k] == 0.) {
+            nzero++;
+        }
+        
+        numimgs++;
+        if (GetKeyDbl (&hdr, "EXPEND", USE_DEFAULT, 0., &keyend) != 0) {
+            sprintf (MsgText,
+		     "cannot read 'EXPEND' from the primary header of '%s'",
+		     fdata);
+            trlerror (MsgText);
+            freeHdr (&hdr);
+            return(status = KEYWORD_MISSING);
+        }
+        if (GetKeyDbl (&hdr, "EXPSTART", USE_DEFAULT, 0., &keystart) != 0) {
+            sprintf (MsgText,
+		     "cannot read 'EXPSTART' from the primary header of '%s'",
+		     fdata);
+            trlerror (MsgText);
+            freeHdr (&hdr);
+            return(status = KEYWORD_MISSING);
+        }
+        
+        end = (keyend > end) ? keyend: end;
+	start = (keystart < start) ? keystart : start;
+        closeImage (ip);
+        freeHdr (&hdr);
+
+	/* Open first SCI extension header to read BUNIT keyword */
+        ip = openInputImage (fdata, "SCI", 1);
+        if (hstio_err()) {
+            sprintf (MsgText, "Cannot open data file '%s'", fdata);
+            trlerror (MsgText);
+            return (status = OPEN_FAILED);
+        }
+
+        initHdr (&hdr);
+
+        /* read in the extension header from image */
+        getHeader (ip, &hdr);
+
+        /* Read the BUNIT keyword */
+        units[0] = '\0';
+        if (getKeyS (&hdr, "BUNIT", units)) {
+            trlkwerr ("BUNIT", fdata);
+            return (status = 1);
+        }
+
+        /* Check the BUNIT keyword value for validity */
+        if (streq_ic (units,"COUNTS/S") || streq_ic (units, "ELECTRONS/S"))
+            bunit[k] = COUNTRATE;
+        else if  (streq_ic (units,"COUNTS") || streq_ic (units, "ELECTRONS"))
+            bunit[k] = COUNTS;
+        else {
+            sprintf (MsgText, "%s value for BUNIT does not match", units);
+            trlerror (MsgText);
+            return (status = INVALID_VALUE);
+        }
+
+
+        closeImage (ip);
+        freeHdr (&hdr);
+    }
+    
+    if (nzero > 0 && nzero < *nimgs) {
+        trlwarn ("Some (but not all) input imsets have zero exposure time.");
+	trlwarn ("Final product will be compromised!");
+
+	/* This type of error will need to be handled differently in order
+	** to allow pipeline processing of this type of dataset.
+        return (status = INVALID_VALUE);
+	*/
+    }
+    
+    /* Only return the number of valid input images,
+    ** initial EXPSTART and final EXPEND value */
+    *nimgs = numimgs;
+    *expend = end;
+    *expstart = start;
+    
+    return (status);
+}
