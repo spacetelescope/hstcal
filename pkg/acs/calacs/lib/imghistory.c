7e25b12246b835e205661776dac2acb48f426b14
cslocum <cslocum@stsci.edu>
2014-03-14 00:40:38 +0000

    automatic ssbx update
    git-svn-id: http://svn.stsci.edu/svn/ssb/stsci_python/hstcal/branches/irafx@30349 fe389314-cf27-0410-b35b-8c050e845b92

---
+++ pkg/acs/calacs/lib/imghistory.c
@@ -0,0 +1,45 @@
+# include <stdio.h>
+# include <string.h>
+# include "hstio.h"
+# include "acs.h"
+# include "acserr.h"
+
+/* This routine writes history records for a reference image, including
+   the name of the file and the pedigree and descrip values.
+*/
+
+int ImgHistory (RefImage *ref, Hdr *phdr) {
+
+/* arguments:
+RefImage *ref     i: info about reference image
+Hdr *phdr         io: header to receive history records
+*/
+
+	extern int status;
+
+	char history[ACS_LINE];
+
+	strcpy (history, "  reference image ");
+	strcat (history, ref->name);
+	addHistoryKw (phdr, history);
+	if (hstio_err())
+	    return (status = HEADER_PROBLEM);
+
+	if (ref->pedigree[0] != '\0') {
+	    strcpy (history, "    ");
+	    strcat (history, ref->pedigree);
+	    addHistoryKw (phdr, history);
+	    if (hstio_err())
+		return (status = HEADER_PROBLEM);
+	}
+
+	if (ref->descrip[0] != '\0') {
+	    strcpy (history, "    ");
+	    strcat (history, ref->descrip);
+	    addHistoryKw (phdr, history);
+	    if (hstio_err())
+		return (status = HEADER_PROBLEM);
+	}
+
+	return (status);
+}
