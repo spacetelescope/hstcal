c7a50973cbc6a88847fe0977bd0c29505e6b1ffe
cslocum <cslocum@stsci.edu>
2014-04-11 00:10:28 +0000

    automatic ssbx update
    git-svn-id: http://svn.stsci.edu/svn/ssb/stsci_python/hstcal/branches/irafx@30930 fe389314-cf27-0410-b35b-8c050e845b92

---
+++ pkg/acs/calacs/include/acsrej.h
@@ -0,0 +1,38 @@
+/* The header file acs.h must be called prior to calling this
+    file, in order to define the macros used here.
+*/
+
+/* define the user input parameter constants */
+# define    TOTAL       0
+# define    SCALENSE    1
+# define    INITGUES    2
+# define    SKYSUB      3
+# define    CRSIGMAS    4
+# define    CRRADIUS    5
+# define    CRTHRESH    6
+# define    BADINPDQ    7
+# define    CRMASK      8
+# define    MAX_PAR     8
+
+/*  define the parameter structure */
+typedef struct {
+    char    tbname[ACS_FNAME];      /* Name of CCDTAB to be read */
+    float   radius;
+    float   thresh;
+    int     nexpnames;
+    float   meanexp;
+    float   fillval;
+    char    sigmas[SZ_STRKWVAL];
+    char    initgues[ACS_FITS_REC];
+    char    sky[ACS_FITS_REC];
+    float   scalense;
+    char    expname[ACS_FITS_REC];
+    char    skyname[ACS_FITS_REC];
+    int     mask;
+    short   crval;
+    short   badinpdq;
+    int     shadcorr;
+    int     printtime;
+    int     verbose;
+    int     newbias;
+} clpar;
