9358d4b5767d62522b9f4c3a17df9397aeb976c3
cslocum <cslocum@stsci.edu>
2014-09-26 00:11:44 +0000

    automatic ssbx update
    git-svn-id: http://svn.stsci.edu/svn/ssb/stsci_python/hstcal/branches/irafx@34550 fe389314-cf27-0410-b35b-8c050e845b92

---
+++ pkg/stis/calstis/cs1/doshad.c
@@ -0,0 +1,105 @@
+# include <stdio.h>
+
+# include "c_iraf.h"
+# include "hstio.h"
+# include "stis.h"
+# include "calstis1.h"
+# include "stiserr.h"
+# include "stisdef.h"
+
+/* This routine applies the shutter shading correction:
+
+	corrected = uncalibrated * EXPTIME / (EXPTIME + SHADFILE)
+
+   The value of EXPTIME will (locally) be divided by the number of
+   exposures that were combined by cosmic-ray rejection to make the
+   current image.
+*/
+
+int doShad (StisInfo1 *sts, SingleGroup *x) {
+
+/* arguments:
+StisInfo1 *sts     i: calibration switches, etc
+SingleGroup *x    io: image to be calibrated; written to in-place
+*/
+
+	int status;
+
+	SingleGroup y, z;	/* y and z are scratch space */
+	float exptime;		/* exposure time / ncombine */
+	int extver = 1;		/* get this imset from shutter shading image */
+	int rx, ry;		/* for binning dark down to size of x */
+	int x0, y0;		/* offsets of sci image */
+	int same_size;		/* true if no binning of ref image required */
+	int high_res;		/* true if high-res pixels in dispersion dir */
+	int avg = 1;		/* bin2d should average within each bin */
+
+	int FindBin (StisInfo1 *, SingleGroup *, SingleGroup *,
+		int *, int *, int *, int *, int *, int *);
+
+	if (sts->shadcorr != PERFORM)
+	    return (0);
+
+	if (sts->exptime <= 0.) {
+	    printf (
+		"Warning  EXPTIME must be positive if SHADCORR = PERFORM.\n");
+	    sts->shadcorr = IGNORED;
+	    return (0);
+	}
+
+	initSingleGroup (&y);
+
+	/* Correct for the number of exposures that were combined. */
+	exptime = sts->exptime / sts->ncombine;
+
+	/* Get the shutter shading image data. */
+	getSingleGroup (sts->shad.name, extver, &y);
+	if (hstio_err())
+	    return (OPEN_FAILED);
+
+	/* Compare binning of science image and reference image;
+	   get the same_size flag, and get info about binning and offset
+	   for use by bin2d.
+	*/
+	if (status = FindBin (sts, x, &y,
+		&same_size, &high_res, &rx, &ry, &x0, &y0))
+	    return (status);
+
+	/* Apply the shutter shading image to the input image. */
+
+	if (same_size) {
+
+	    /* No binning required. */
+
+	    if (status = multk2d (&y, 1. / exptime))
+		return (status);
+	    if (status = addk2d (&y, 1.))
+		return (status);
+	    if (status = div2d (x, &y)) {
+		printf ("ERROR    (doShad) size mismatch.\n");
+		return (status);
+	    }
+	    freeSingleGroup (&y);
+
+	} else {
+
+	    /* Bin the reference image down to the actual size of x. */
+
+	    initSingleGroup (&z);
+	    allocSingleGroup (&z, x->sci.data.nx, x->sci.data.ny);
+	    if (status = bin2d (&y, x0, y0, rx, ry, avg, &z)) {
+		printf ("ERROR    (doShad) size mismatch.\n");
+		return (status);
+	    }
+	    freeSingleGroup (&y);			/* done with y */
+	    if (status = multk2d (&z, 1. / exptime))
+		return (status);
+	    if (status = addk2d (&z, 1.))
+		return (status);
+	    if (status = div2d (x, &z))
+		return (status);
+	    freeSingleGroup (&z);			/* done with z */
+	}
+
+	return (0);
+}
---
+++ pkg/stis/calstis/lib/rowpedigree.c
@@ -0,0 +1,56 @@
+# include <stdio.h>
+# include <string.h>
+# include "c_iraf.h"
+# include "hstio.h"
+# include "xtables.h"
+
+# include "stis.h"
+# include "stiserr.h"
+# include "stisdef.h"
+
+/* This routine gets pedigree and descrip from the current table row.
+
+   The pedigree and descrip columns need not be defined.  If not, this
+   is flagged by their column pointers being zero, in which case no change
+   will be made to the ref struct for this table.
+
+   If the pedigree column is present, the value in the current row
+   will replace any value previously gotten from the header.  As with
+   the value from the header, if the first five letters of pedigree are
+   DUMMY, then goodPedigree will be set to DUMMY_PEDIGREE; if not,
+   goodPedigree will not be changed from its previous value (either as
+   initialized or as gotten from the header).
+
+   If the descrip column is present, the value is assigned to a second
+   string, descrip2, rather than overwriting the one from the header.
+   If the column is not found, descrip2 will be set to null.
+*/
+
+int RowPedigree (RefTab *ref, int row,
+	IRAFPointer tp, IRAFPointer cp_pedigree, IRAFPointer cp_descrip) {
+
+	/* Get pedigree and descrip.  If either or both are missing,
+	   that's not an error in this case.
+	*/
+	if (cp_pedigree > 0) {
+	    c_tbegtt (tp, cp_pedigree, row, ref->pedigree, STIS_FITS_REC);
+	    if (c_iraferr())
+		return (TABLE_ERROR);
+	    /* Is this row flagged as dummy? */
+	    if (strncmp (ref->pedigree, "DUMMY", 5) == 0)
+		ref->goodPedigree = DUMMY_PEDIGREE;
+	    else
+		ref->goodPedigree = GOOD_PEDIGREE;
+	}
+
+	if (cp_descrip > 0) {
+	    c_tbegtt (tp, cp_descrip, row, ref->descrip2, STIS_FITS_REC);
+	    if (c_iraferr())
+		return (TABLE_ERROR);
+	} else {
+	    ref->descrip2[0] = '\0';
+	}
+
+
+	return (0);
+}
