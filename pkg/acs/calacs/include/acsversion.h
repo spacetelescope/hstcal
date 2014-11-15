c937bd233b65833ef59251de89d36feb86aeff64
cslocum <cslocum@stsci.edu>
2014-11-15 01:43:07 +0000

    automatic ssbx update
    git-svn-id: http://svn.stsci.edu/svn/ssb/stsci_python/hstcal/branches/irafx@35437 fe389314-cf27-0410-b35b-8c050e845b92

---
+++ pkg/acs/calacs/include/acsversion.h
@@ -0,0 +1,10 @@
+/* This string is written to the output primary header as CAL_VER. */
+
+
+#define ACS_CAL_VER "8.3.1 (10-Oct-2014)"
+#define ACS_CAL_VER_NUM "8.3.1"
+
+
+/* name and version number of the CTE correction algorithm */
+#define ACS_CTE_NAME "PixelCTE 2012"
+#define ACS_CTE_VER "3.3"
