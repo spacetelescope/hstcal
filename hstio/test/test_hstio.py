3923fbf6252186c1593c3e73bf430de0b0ce62cb
cslocum <cslocum@stsci.edu>
2014-11-22 01:43:11 +0000

    automatic ssbx update
    git-svn-id: http://svn.stsci.edu/svn/ssb/stsci_python/hstcal/branches/irafx@35651 fe389314-cf27-0410-b35b-8c050e845b92

---
+++ hstio/test/test_hstio.py
@@ -0,0 +1,35 @@
+# Run with nosetests
+import os
+import platform
+import pyfits
+import shutil
+
+path = os.path.dirname(__file__)
+bin_path = os.path.join(path, "../../build.%s/hstio/test/" % platform.platform())
+join = os.path.join
+
+def test_hstio_resize_hdu():
+    shutil.copyfile(join(path, "test_hstio_resize_hdu.fits"),
+                    join(path, "test_hstio_resize_hdu_tmp.fits"))
+
+    assert os.system("%s %s" % (join(bin_path, "test_hstio_resize_hdu"),
+                                join(path, "test_hstio_resize_hdu_tmp.fits"))) == 0
+
+    hdulist = pyfits.open(join(path, "test_hstio_resize_hdu_tmp.fits"))
+    assert hdulist[1].data is not None
+    print hdulist[1].data[34, 12]
+    assert hdulist[1].data[34,12] == 43
+
+def test_hstio_resize_hdu2():
+    shutil.copyfile(join(path, "test_hstio_resize_hdu.fits"),
+                    join(path, "test_hstio_resize_hdu_tmp.fits"))
+
+    assert os.system("%s %s" % (join(bin_path, "test_hstio_resize_hdu2"),
+                                join(path, "test_hstio_resize_hdu_tmp.fits"))) == 0
+
+    hdulist = pyfits.open(join(path, "test_hstio_resize_hdu_tmp.fits"))
+    assert hdulist[1].data is not None
+    assert hdulist[1].data[1000,50] == 50
+    print hdulist[1].data[42, 42]
+    assert hdulist[1].data[42, 42] == 42
+
