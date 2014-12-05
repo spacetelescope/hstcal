a470e0a27b734f97c6856503af540f121c5f2f83
cslocum <cslocum@stsci.edu>
2014-12-05 01:05:37 +0000

    delete for automatic ssbx update
    git-svn-id: http://svn.stsci.edu/svn/ssb/stsci_python/hstcal/branches/irafx@35984 fe389314-cf27-0410-b35b-8c050e845b92

--- pkg/stis/calstis/include/stisomit.h
+++
@@ -1,7 +0,0 @@
-/* These are the calibration steps that by default are not performed.
-   This file is included only by lib/defswitch.c.
-*/
-
-# define STIS_N_OMIT   3
-
-static char omitsw[STIS_N_OMIT][9] = {"atodcorr", "shadcorr", "sgeocorr"};
