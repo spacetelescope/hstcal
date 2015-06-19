87946526ffae25c9ba514e454d6d6a31e23ad2b7
cslocum <cslocum@stsci.edu>
2015-06-19 00:21:29 +0000

    automatic ssbx update
    git-svn-id: http://svn.stsci.edu/svn/ssb/stsci_python/hstcal/branches/irafx@41313 fe389314-cf27-0410-b35b-8c050e845b92

---
+++ tables/c_phopar.c
@@ -0,0 +1,8 @@
+void c_phopar (int nwave, float *wavelength, float *throughput, float *phot) {
+
+        /* these are dummy values */
+        phot[0] = 1.;
+        phot[1] = 1.;
+        phot[2] = 1.;
+        phot[3] = 1.;
+}
