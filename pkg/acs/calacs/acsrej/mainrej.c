179665b6fc62b8e25e703c7e200c4dc7b431e0ed
lim <lim@stsci.edu>
2015-07-27 20:41:44 +0000

    Fix for #1175
    git-svn-id: http://svn.stsci.edu/svn/ssb/stsci_python/hstcal/trunk@42539 fe389314-cf27-0410-b35b-8c050e845b92

--- pkg/acs/calacs/acsrej/mainrej.c
+++ pkg/acs/calacs/acsrej/mainrej.c
@@ -10,16 +10,19 @@
 # include "acserr.h"
 # include "acsrej.h"
 
+static void FreeNames (char *);
+
 int status = 0;             /* zero is OK */
+
 
 int main (int argc, char **argv) {
 
-    char    input[ACS_LINE*2], output[ACS_LINE];    /* file names */
+    char    *input, output[ACS_LINE];    /* file names */
     clpar   par;                                    /* parameters used */
     int     newpar[MAX_PAR+1];          /* user specifiable parameters */
     char    mtype[SZ_STRKWVAL+1];      /* Role of exposure in association */
 
-    int rej_command (int, char **, char *, char *, clpar *, int []);
+    int rej_command (int, char **, char **, char *, clpar *, int []);
     int AcsRej (char *, char *, char *, clpar *, int []);
     void WhichError (int);
 
@@ -28,8 +31,12 @@ int main (int argc, char **argv) {
     /* Initialize mtype to NULL to signal no change in ASN_MTYP for output*/
     mtype[0] = '\0';
 
+    /* Allocate later */
+    input = NULL;
+
     /* Get input and output file names and switches in the command line. */
-    if (rej_command (argc, argv, input, output, &par, newpar)) {
+    if (rej_command (argc, argv, &input, output, &par, newpar)) {
+        FreeNames(input);
         exit (ERROR_RETURN);
     }
 
@@ -46,7 +53,16 @@ int main (int argc, char **argv) {
 
     if (status) {
         WhichError (status);
+        FreeNames(input);
         exit (ERROR_RETURN);
-    } else
+    } else {
+        FreeNames(input);
         exit (0);
+    }
+}
+
+
+static void FreeNames (char *input) {
+    if (input != NULL)
+        free(input);
 }
--- pkg/acs/calacs/acsrej/rej_command.c
+++ pkg/acs/calacs/acsrej/rej_command.c
@@ -8,41 +8,42 @@
 # include "acsrej.h"
 # include "acserr.h"
 
-void rej_reset (clpar *, int []); 
+void rej_reset (clpar *, int []);
 static int syntax_error (char *);
 static int getArgS (char **, int, int *, short *);
 static int getArgR (char **, int, int *, float *);
 static int getArgT (char **, int, int *, char *);
 
-/*  
+/*
    Processes command-line parameters for AcsRej.
 
    Revision history:
    ----------------
    27 Aug 98  -  Adapted from CALSTIS2 cs2_command.c (WJ Hack)
+   27 Jul 15  -  Dynamic size for input filenames (PL Lim)
 */
 
-int rej_command (int argc, char **argv, char *input, char *output,
-                clpar *par, int newpar[]) 
+int rej_command (int argc, char **argv, char **input, char *output,
+                clpar *par, int newpar[])
 {
 
 /* arguments
 int argc;           i: input command-line parameters
 char **argv;
-char *input;        o: input file name or file list
+char **input;       o: input file name or file list
 char *output;       o: output file name
 clpar *par;         o: user specified parameters
 int newpar[];       o: array of parameters set by the user
 */
 
     extern int status;
-    
+
     int ctoken;     /* current command-line token being processed */
 
     /* reset the parameters */
-    rej_reset (par, newpar); 
+    rej_reset (par, newpar);
 
-    /* not enough arguments 
+    /* not enough arguments
         List all options for user
     */
     if (argc < 3) {
@@ -51,12 +52,17 @@ int newpar[];       o: array of parameters set by the user
         printf("                    [-table <filename>] [-scale #]\n");
         printf("                    [-init (med|min)] [-sky (none|mode)]\n");
         printf("                    [-sigmas #] [-radius #] [-thresh #]\n");
-        printf("                    [-pdq #]\n"); 
+        printf("                    [-pdq #]\n");
         return(status);
     }
 
-    /* Get names of input and output files. These are mandatory. */
-    strcpy (input,  argv[1]);
+    /* Get names of input and output files. These are mandatory.
+      Input list is also limited by tables/c_imt.c */
+    if ((*input = calloc(strlen(argv[1]) + 1, sizeof(char))) == NULL) {
+        printf ("Can't even begin; out of memory.\n");
+        return (status = OUT_OF_MEMORY);
+    }
+    strcpy (*input, argv[1]);
     strcpy (output, argv[2]);
 
     /* Scan remaining parameters. */
@@ -71,23 +77,23 @@ int newpar[];       o: array of parameters set by the user
 
             /* These do not require additional arguments. */
 
-            if (strcmp("t", argv[ctoken]+1) == 0) { 
+            if (strcmp("t", argv[ctoken]+1) == 0) {
                 par->printtime = 1;
                 ctoken++;
 
-            } else if (strcmp("v", argv[ctoken]+1) == 0) { 
+            } else if (strcmp("v", argv[ctoken]+1) == 0) {
                 par->verbose = 1;
                 ctoken++;
 
-            } else if (strcmp("shadcorr", argv[ctoken]+1) == 0) { 
+            } else if (strcmp("shadcorr", argv[ctoken]+1) == 0) {
                 par->shadcorr = 1;
                 ctoken++;
 
-            } else if (strcmp("newbias", argv[ctoken]+1) == 0) { 
+            } else if (strcmp("newbias", argv[ctoken]+1) == 0) {
                 par->newbias = 1;
                 ctoken++;
 
-            } else if (strcmp("crmask", argv[ctoken]+1) == 0) { 
+            } else if (strcmp("crmask", argv[ctoken]+1) == 0) {
                 par->mask = 1;
                 newpar[CRMASK] = 1;
                 newpar[TOTAL]++;
@@ -97,47 +103,47 @@ int newpar[];       o: array of parameters set by the user
                handled by the getArg functions.
             */
 
-            } else if (strcmp("table", argv[ctoken]+1) == 0) { 
+            } else if (strcmp("table", argv[ctoken]+1) == 0) {
                 if (getArgT (argv, argc, &ctoken, par->tbname))
                     return (status = INVALID_VALUE);
 
-            } else if (strcmp("scale", argv[ctoken]+1) == 0) { 
+            } else if (strcmp("scale", argv[ctoken]+1) == 0) {
                 newpar[TOTAL]++;
                 newpar[SCALENSE] = 1;
                 if (getArgR (argv, argc, &ctoken, &par->scalense))
                     return (status = INVALID_VALUE);
 
-            } else if (strcmp("init", argv[ctoken]+1) == 0) { 
+            } else if (strcmp("init", argv[ctoken]+1) == 0) {
                 newpar[TOTAL]++;
                 newpar[INITGUES] = 1;
                 if (getArgT (argv, argc, &ctoken, par->initgues))
                     return (status = INVALID_VALUE);
 
-            } else if (strcmp("sky", argv[ctoken]+1) == 0) { 
+            } else if (strcmp("sky", argv[ctoken]+1) == 0) {
                 newpar[TOTAL]++;
                 newpar[SKYSUB] = 1;
                 if (getArgT (argv, argc, &ctoken, par->sky))
                     return (status = INVALID_VALUE);
 
-            } else if (strcmp("sigmas", argv[ctoken]+1) == 0) { 
+            } else if (strcmp("sigmas", argv[ctoken]+1) == 0) {
                 newpar[TOTAL]++;
                 newpar[CRSIGMAS] = 1;
                 if (getArgT (argv, argc, &ctoken, par->sigmas))
                     return (status = INVALID_VALUE);
 
-            } else if (strcmp("radius", argv[ctoken]+1) == 0) { 
+            } else if (strcmp("radius", argv[ctoken]+1) == 0) {
                 newpar[TOTAL]++;
                 newpar[CRRADIUS] = 1;
                 if (getArgR (argv, argc, &ctoken, &par->radius))
                     return (status = INVALID_VALUE);
 
-            } else if (strcmp("thresh", argv[ctoken]+1) == 0) { 
+            } else if (strcmp("thresh", argv[ctoken]+1) == 0) {
                 newpar[TOTAL]++;
                 newpar[CRTHRESH] = 1;
                 if (getArgR (argv, argc, &ctoken, &par->thresh))
                     return (status = INVALID_VALUE);
 
-            } else if (strcmp("pdq", argv[ctoken]+1) == 0) { 
+            } else if (strcmp("pdq", argv[ctoken]+1) == 0) {
                 newpar[TOTAL]++;
                 newpar[BADINPDQ] = 1;
                 if (getArgS (argv, argc, &ctoken, &par->badinpdq))
@@ -146,7 +152,7 @@ int newpar[];       o: array of parameters set by the user
             /* No match. */
             } else
                 return (syntax_error (argv[ctoken]));
-            } 
+            }
         }
     }
 
@@ -163,7 +169,7 @@ int newpar[];       o: array of parameters set by the user
 
 static int getArgS (char **argv, int argc, int *ctoken, short *value) {
     extern int status;
-    
+
     if (*ctoken <= (argc-2)) {
         *value = (short) strtol(argv[++(*ctoken)], (char **)NULL, 10);
         (*ctoken)++;
@@ -181,7 +187,7 @@ static int getArgS (char **argv, int argc, int *ctoken, short *value) {
 
 static int getArgT (char **argv, int argc, int *ctoken, char *value) {
     extern int status;
-    
+
     if (*ctoken <= (argc-2)) {
         strcpy (value, argv[++(*ctoken)]);
         (*ctoken)++;
@@ -199,7 +205,7 @@ static int getArgT (char **argv, int argc, int *ctoken, char *value) {
 
 static int getArgR (char **argv, int argc, int *ctoken, float *value) {
     extern int status;
-    
+
     if (*ctoken <= (argc-2)) {
         *value = strtod (argv[++(*ctoken)], (char **)NULL);
         (*ctoken)++;
@@ -225,7 +231,7 @@ static int syntax_error (char *msg) {
 /* ------------------------------------------------------------------*/
 
 
-void rej_reset (clpar *par, int newpar[]) 
+void rej_reset (clpar *par, int newpar[])
 {
 
 /* arguments
