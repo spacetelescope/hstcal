3dd82bc1dab589b7282faba9b72976b99e504181
lim <lim@stsci.edu>
2016-01-14 21:07:04 +0000

    Updated version, release docs, and acsrej_loop history.
    git-svn-id: http://svn.stsci.edu/svn/ssb/stsci_python/hstcal/branches/lim-calacs-ticket976@48017 fe389314-cf27-0410-b35b-8c050e845b92

--- pkg/acs/calacs/include/acsversion.h
+++ pkg/acs/calacs/include/acsversion.h
@@ -1,5 +1,5 @@
 /* This string is written to the output primary header as CAL_VER. */
-#define ACS_CAL_VER "8.4.0 (13-Jan-2016)"
+#define ACS_CAL_VER "8.4.0 (14-Jan-2016)"
 #define ACS_CAL_VER_NUM "8.4.0"
 
 /* name and version number of the CTE correction algorithm */
