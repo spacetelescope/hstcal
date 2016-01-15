6984ecaa5084ba1293d6c2583a395ebdf963b777
lim <lim@stsci.edu>
2016-01-15 17:44:02 +0000

    Updated version num and change logs. Cleaned up dodark.c before applying actual algorithm change.
    git-svn-id: http://svn.stsci.edu/svn/ssb/stsci_python/hstcal/branches/lim-calacs-ticket1167@48129 fe389314-cf27-0410-b35b-8c050e845b92

--- pkg/acs/calacs/include/acsversion.h
+++ pkg/acs/calacs/include/acsversion.h
@@ -1,6 +1,6 @@
 /* This string is written to the output primary header as CAL_VER. */
-#define ACS_CAL_VER "8.3.4 (07-Jul-2016)"
-#define ACS_CAL_VER_NUM "8.3.4"
+#define ACS_CAL_VER "8.5.1 (15-Jan-2016)"
+#define ACS_CAL_VER_NUM "8.5.1"
 
 /* name and version number of the CTE correction algorithm */
 #define ACS_CTE_NAME "PixelCTE 2012"
