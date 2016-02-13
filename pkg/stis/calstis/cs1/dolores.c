ee445e22e65f61bff0d41f36511f687b15a747e4
cslocum <cslocum@stsci.edu>
2016-02-13 03:30:59 +0000

    automatic ssbx update
    git-svn-id: http://svn.stsci.edu/svn/ssb/stsci_python/hstcal/branches/irafx@49250 fe389314-cf27-0410-b35b-8c050e845b92

---
+++ pkg/stis/calstis/cs1/dolores.c
@@ -0,0 +1,90 @@
+# include <stdio.h>
+
+# include "c_iraf.h"
+# include "hstio.h"
+# include "stis.h"
+# include "calstis1.h"
+# include "stiserr.h"
+# include "stisdef.h"
+
+/* This routine bins the input image so that BINX = 2 and BINY = 2.
+   The values are added, not averaged.
+   On input, it may be that one of BINX or BINY is already 2; that's OK.
+   If both BINX and BINY are already 2, however, this routine sets done to
+   false and does NOT reallocate x.
+   The coordinate parameters, including LTVi and LTMi_i, will be updated
+   in the extension headers.
+   NOTE:  The input x will be freed and reallocated if we actually do
+   change the binning.
+
+   Phil Hodge, 1998 Oct 5:
+	Change status value 1001 to GENERIC_ERROR_CODE.
+
+   Phil Hodge, 1999 Nov 2:
+	Second argument is now SingleGroup **x instead of SingleGroup *x.
+	Remove SingleGroup *y from calling sequence.
+*/
+
+int doLoRes (StisInfo1 *sts, SingleGroup **x, int *done) {
+
+/* arguments:
+StisInfo1 *sts    i: calibration switches, etc
+SingleGroup **x  io: input image to be binned; output binned image
+int *done         o: true if input image has been binned
+*/
+
+	SingleGroup *in;	/* local variable, for cleaner notation */
+	SingleGroup *out;	/* binned copy of x */
+	int status;
+
+	/* Factors by which we compress X and Y.  These are passed to bin2d,
+	   and they are also used to keep track of which axes were binned.
+	*/
+	int mx, my;
+
+	/* This is a flag to bin2d meaning sum the values, don't average. */
+	int avg = 0;
+
+	*done = 0;				/* initial value */
+
+	in = *x;		/* just for convenience of notation */
+
+	if (sts->bin[0] == 2 && sts->bin[1] == 2)
+	    return (0);
+
+	if (sts->bin[0] < 1 || sts->bin[1] < 1) {
+	    printf ("ERROR    (doLoRes) bin size is less than one.\n");
+	    return (GENERIC_ERROR_CODE);
+	}
+	if (sts->bin[0] > 2 || sts->bin[1] > 2) {
+	    printf ("ERROR    (doLoRes) bin size is greater than two.\n");
+	    return (GENERIC_ERROR_CODE);
+	}
+
+	mx = (sts->bin[0] == 1) ? 2 : 1;
+	my = (sts->bin[1] == 1) ? 2 : 1;
+
+	/* Create a new data set of the appropriate (smaller) size. */
+	if ((out = calloc (1, sizeof (SingleGroup))) == NULL)
+	    return (OUT_OF_MEMORY);
+	initSingleGroup (out);
+	allocSingleGroup (out, (in->sci.data.nx) / mx, (in->sci.data.ny) / my);
+	if (hstio_err())
+	    return (OUT_OF_MEMORY);
+
+	/* Bin the data, putting the result in out. */
+	if ((status = bin2d (in, 0, 0, mx, my, avg, out)))
+	    return (status);
+
+	/* Free x, and copy out to x. */
+	freeSingleGroup (*x);
+	free (*x);
+	*x = out;
+
+	/* Update values related to binning in the sts struct. */
+	sts->bin[0] = 2;
+	sts->bin[1] = 2;
+
+	*done = 1;			/* yes, we binned the image */
+	return (0);
+}
---
+++ pkg/stis/calstis/cs6/idtalg/x1dtable.c
@@ -0,0 +1,188 @@
+# include <stdio.h>
+# include <stdlib.h>
+
+# include "xtables.h"
+
+# include "stis.h"
+# include "stiserr.h"
+# include "calstis6.h"
+
+/*
+   GetX1DTable -- Read X1D table from file.
+
+   Data arrays must be allocated by caller with AllocX1DTable,
+   and fred with FreeX1DTable.
+
+
+   Revision history:
+   ----------------
+   21 Feb 00  -  Implemented (I.Busko)
+*/
+
+int GetX1DTable (TblDesc *tabptr, RowContents **x1d) {
+
+/* arguments
+TblDesc *tabptr;	o: _x1d temporary table
+RowContents **x1d;	o: row contents
+*/
+	int irow, i;
+	int nwave, ngross, nnet, nextrlocy;
+
+	void FreeX1DTable (RowContents **, int);
+
+	/* Set up pointers to columns. */
+
+	c_tbcfnd1 (tabptr->tp, SPORDER,    &tabptr->sporder);
+	c_tbcfnd1 (tabptr->tp, NELEM,      &tabptr->npts);
+	c_tbcfnd1 (tabptr->tp, WAVELENGTH, &tabptr->wave);
+	c_tbcfnd1 (tabptr->tp, GROSS,      &tabptr->gross);
+	c_tbcfnd1 (tabptr->tp, NET,        &tabptr->net);
+	c_tbcfnd1 (tabptr->tp, EXTRLOCY,   &tabptr->extrlocy);
+	if ((tabptr->sporder    == 0) || (tabptr->npts == 0) ||
+            (tabptr->wave == 0) || (tabptr->gross == 0) ||
+            (tabptr->net  == 0)) {
+	    FreeX1DTable (x1d, tabptr->nrows);
+	    c_tbtclo (tabptr->tp);
+	    printf ("Column not found in input table.\n");
+	    return (TABLE_ERROR);
+	}
+
+	/* Alloc memory and read data from each row.
+
+           This is an array of RowContents data structures with
+           tabptr->nrows elements. Each element holds data arrays of
+           tabptr->npts length.
+        */
+
+	for (irow = 0; irow < tabptr->nrows; irow++) {
+	    c_tbegts (tabptr->tp, tabptr->sporder, irow+1,
+                      &(x1d[irow]->sporder));
+	    c_tbegts (tabptr->tp, tabptr->npts, irow+1,
+                      &(x1d[irow]->npts));
+
+	    x1d[irow]->wave     = (double *) calloc (x1d[irow]->npts,
+                                                        sizeof(double));
+	    x1d[irow]->gross    = (float *) calloc (x1d[irow]->npts,
+                                                        sizeof(float));
+	    x1d[irow]->net      = (float *) calloc (x1d[irow]->npts,
+                                                        sizeof(float));
+	    x1d[irow]->extrlocy = (float *) calloc (x1d[irow]->npts,
+                                                        sizeof(float));
+	    if (x1d[irow]->wave == NULL || x1d[irow]->gross    == NULL ||
+                x1d[irow]->net  == NULL || x1d[irow]->extrlocy == NULL) {
+	        FreeX1DTable (x1d, tabptr->nrows);
+	        c_tbtclo (tabptr->tp);
+                printf ("Not enough memory to allocate data arrays.\n");
+	        return (OUT_OF_MEMORY);
+	    }
+	    nwave     = c_tbagtd (tabptr->tp, tabptr->wave, irow+1,
+                                  x1d[irow]->wave, 1, x1d[irow]->npts);
+	    ngross    = c_tbagtr (tabptr->tp, tabptr->gross, irow+1,
+                                  x1d[irow]->gross, 1, x1d[irow]->npts);
+	    nnet      = c_tbagtr (tabptr->tp, tabptr->net, irow+1,
+                                  x1d[irow]->net, 1, x1d[irow]->npts);
+	    nextrlocy = c_tbagtr (tabptr->tp, tabptr->extrlocy, irow+1,
+                                  x1d[irow]->extrlocy,1,x1d[irow]->npts);
+	    if ((nwave     != x1d[irow]->npts) ||
+                (ngross    != x1d[irow]->npts) ||
+                (nnet      != x1d[irow]->npts) ||
+                (nextrlocy != x1d[irow]->npts)) {
+	        FreeX1DTable (x1d, tabptr->nrows);
+	        c_tbtclo (tabptr->tp);
+	        printf ("Insufficent number of elements read from table.\n");
+	        return (TABLE_ERROR);
+	    }
+
+	    /* Compatibilize extrlocy with 0-indexed image arrays. */
+
+	    for (i = 0; i < x1d[irow]->npts; x1d[irow]->extrlocy[i++] -= 1.0);
+
+	}
+
+	return (0);
+}
+
+
+
+/*  Opens the X1D table and gets number of rows. */
+
+int OpenX1DTable (char *table, int write, TblDesc *tabptr) {
+
+/* arguments
+char *table;		i: input table name
+int write;		i: read only or read/write ?
+TblDesc *tabptr;	o: _x1d temporary table
+*/
+	if (write)
+	    tabptr->tp = c_tbtopn (table, IRAF_READ_WRITE, 0);
+	else
+	    tabptr->tp = c_tbtopn (table, IRAF_READ_ONLY, 0);
+
+	if (c_iraferr()) {
+	    printf ("_x1d temporary table could not be opened.\n");
+	    return (OPEN_FAILED);
+	}
+
+	tabptr->nrows = c_tbpsta (tabptr->tp, TBL_NROWS);
+	if (c_iraferr()) {
+	    c_tbtclo (tabptr->tp);
+            printf ("Cannot determine the number of rows in table.\n");
+	    return (OPEN_FAILED);
+	}
+	return (0);
+}
+
+
+/* Alloc memory for a X1D table array. */
+
+RowContents **AllocX1DTable (int nrows) {
+
+/* arguments
+int nrows;		i: number of rows
+*/
+	RowContents **x1d;
+	int i;
+
+	x1d = (RowContents **) malloc (nrows * sizeof(RowContents *));
+	if (x1d == NULL) {
+            printf ("Not enough memory to allocate row pointer.\n");
+	    return (NULL);
+	}
+	for (i = 0; i < nrows; i++) {
+	    x1d[i] = (RowContents *) calloc (1, sizeof(RowContents));
+	    if (x1d[i] == NULL) {
+                printf ("Not enough memory to allocate row pointer.\n");
+	        return (NULL);
+	    }
+	    x1d[i]->wave  = NULL; /* not allocated yet ! */
+	    x1d[i]->gross = NULL;
+	}
+	return (x1d);
+}
+
+
+
+/* Frees the data areas used by GetX1DTable() */
+
+void FreeX1DTable (RowContents **x1d, int nrows) {
+
+/* arguments
+RowContents **x1d;	i: row contents
+*/
+	int i;
+
+	if (x1d != NULL) {
+	    for (i = 0; i < nrows; i++) {
+	        if (x1d[i]->gross != NULL)
+	            free (x1d[i]->gross);
+	        if (x1d[i]->wave != NULL) {
+	            free (x1d[i]->extrlocy);
+	            free (x1d[i]->net);
+	            free (x1d[i]->wave);
+	        }
+	        free (x1d[i]);
+	    }
+	    free (x1d);
+	    x1d = NULL;
+	}
+}
