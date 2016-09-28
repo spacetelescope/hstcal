be40072632c6f8302a207da6ebfe73ac91f8a54c
Megan Sosey <sosey@stsci.edu>
2016-09-28 18:21:37 -0400

    one more mem allocation fix for wf3rej

--- pkg/wfc3/calwf3/include/wf3version.h
+++ pkg/wfc3/calwf3/include/wf3version.h
@@ -1,4 +1,4 @@
 /* This string is written to the output primary header as CAL_VER. */
 
-# define WF3_CAL_VER "3.4(26-Sep-2016)"
+# define WF3_CAL_VER "3.4(28-Sep-2016)"
 # define WF3_CAL_VER_NUM "3.4"
