8bcdfed94f74d4ce1901e2c2e4eb0fae7154a3c4
Megan Sosey <sosey@stsci.edu>
2017-03-27 15:50:16 -0400

    photometry keyword updates for wfc3 uvis per https://github.com/spacetelescope/hstcal/issues/67

--- pkg/wfc3/calwf3/include/wf3version.h
+++ pkg/wfc3/calwf3/include/wf3version.h
@@ -1,4 +1,5 @@
 /* This string is written to the output primary header as CAL_VER. */
 
-# define WF3_CAL_VER "3.4.1(10-Apr-2017)"
+
+# define WF3_CAL_VER "3.4.1(Apr-10-2017)"
 # define WF3_CAL_VER_NUM "3.4.1"
