45d7823f466d258481b2cef8c7b5f4828a7cc5ac
Megan Sosey <sosey@stsci.edu>
2017-04-10 10:18:28 -0400

    fixed missed init of header vars in cte code

--- pkg/wfc3/calwf3/include/wf3version.h
+++ pkg/wfc3/calwf3/include/wf3version.h
@@ -1,4 +1,4 @@
 /* This string is written to the output primary header as CAL_VER. */
 
-# define WF3_CAL_VER "3.4.1(02-Apr-2017)"
+# define WF3_CAL_VER "3.4.1(10-Apr-2017)"
 # define WF3_CAL_VER_NUM "3.4.1"
