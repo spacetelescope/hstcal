36b547770e47ce3cbff29e528444b248fb5ac3e0
James Noss <jnoss@stsci.edu>
2017-05-02 15:02:17 -0400

    Revert "BLEVCORR to use fullframe logic on everythiing with new OSCNTAB for WFC and HRC. Fixed BLEVCORR to only use both biassect if they are both truly defined."
    This reverts commit fc9086d5cc63653c099b80c84aaa8929c26faef6.

--- pkg/acs/calacs/include/acsversion.h
+++ pkg/acs/calacs/include/acsversion.h
@@ -1,6 +1,6 @@
 /* This string is written to the output primary header as CAL_VER. */
-#define ACS_CAL_VER "9.0.0 (22-Nov-2016)"
-#define ACS_CAL_VER_NUM "9.0.0"
+#define ACS_CAL_VER "8.3.5 (21-Oct-2016)"
+#define ACS_CAL_VER_NUM "8.3.5"
 
 /* name and version number of the CTE correction algorithm */
 #define ACS_CTE_NAME "PixelCTE 2012"
