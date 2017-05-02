90a3cdab7ea548231c82323aa4a43c7b3014b5df
James Noss <jnoss@stsci.edu>
2017-05-02 15:01:46 -0400

    Revert "No error code when no row found in OSCNTAB"
    This reverts commit f44bb49bf4e6d49a87c6ed1f67c993649607ce67.

--- pkg/acs/calacs/include/acsversion.h
+++ pkg/acs/calacs/include/acsversion.h
@@ -1,5 +1,5 @@
 /* This string is written to the output primary header as CAL_VER. */
-#define ACS_CAL_VER "9.0.0 (15-Dec-2016)"
+#define ACS_CAL_VER "9.0.0 (22-Nov-2016)"
 #define ACS_CAL_VER_NUM "9.0.0"
 
 /* name and version number of the CTE correction algorithm */
