13823cdefbdfc08f2bc33ed3e2ac8837e46b9390
James Noss <jnoss@stsci.edu>
2017-06-01 10:46:09 -0400

    Bump ACS version to 9.2.0
    This updates the following files:
    
    pkg/acs/calacs/Dates
    pkg/acs/calacs/History
    pkg/acs/calacs/Updates
    pkg/acs/calacs/include/acsversion.h
    
    resolves #146
    
    Signed-off-by: James Noss <jnoss@stsci.edu>

--- pkg/acs/calacs/include/acsversion.h
+++ pkg/acs/calacs/include/acsversion.h
@@ -1,6 +1,6 @@
 /* This string is written to the output primary header as CAL_VER. */
-#define ACS_CAL_VER "9.1.0 (21-Feb-2017)"
-#define ACS_CAL_VER_NUM "9.1.0"
+#define ACS_CAL_VER "9.2.0 (01-Jun-2017)"
+#define ACS_CAL_VER_NUM "9.2.0"
 
 /* name and version number of the CTE correction algorithm */
 #define ACS_GEN1_CTE_NAME "PixelCTE 2012"
