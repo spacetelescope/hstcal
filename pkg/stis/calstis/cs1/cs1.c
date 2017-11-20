spacetelescope/hstcal

commit e07f1c99d485fab135690555effdbbcfb0413367

Plug memory leaks in main.c code.
Resolves #239
Fixes #274 (smuggled)

Signed-off-by: James Noss <jnoss@stsci.edu>

pkg/stis/calstis/cs1/cs1.c
@@ -5,6 +5,7 @@
 # include <time.h>
 # include <string.h>
 
+#include "hstcal_memory.h"
 # include "c_iraf.h"		/* for c_irafinit */
 # include "ximio.h"
 
@@ -14,7 +15,6 @@
 # include "hstcalversion.h"
 
 static int CompareNumbers (int, int, char *);
-static void FreeNames (char *, char *, char *, char *, char *, char *);
 static void printSyntax(void)
 {
     printf ("syntax:  cs1.e [--help] [-t] [-v] [--version] [--gitinfo] input output [outblev]\n");
@@ -96,21 +96,30 @@ int main (int argc, char **argv) {
 
 	c_irafinit (argc, argv);
 
+    PtrRegister ptrReg;
+    initPtrRegister(&ptrReg);
 	/* Allocate space for file names. */
 	inlist = calloc (1, sizeof (char));	/* allocated later */
+	addPtr(&ptrReg, inlist, &free);
 	outlist = calloc (1, sizeof (char));
+	addPtr(&ptrReg, outlist, &free);
 	blevlist = calloc (1, sizeof (char));
+	addPtr(&ptrReg, blevlist, &free);
 	input = calloc (STIS_LINE+1, sizeof (char));
+	addPtr(&ptrReg, input, &free);
 	output = calloc (STIS_LINE+1, sizeof (char));
+	addPtr(&ptrReg, output, &free);
 	outblev = calloc (STIS_LINE+1, sizeof (char));
-	if (inlist == NULL || outlist == NULL || blevlist == NULL ||
-		input == NULL || output == NULL || outblev == NULL) {
+	addPtr(&ptrReg, outblev, &free);
+	if (!inlist || !outlist || !blevlist || !input || !output || !outblev) {
 	    printf ("ERROR:  Can't even begin; out of memory.\n");
+	    freeOnExit(&ptrReg);
 	    exit (ERROR_RETURN);
 	}
 
 	/* Initialize the lists of reference file keywords and names. */
 	InitRefFile (&refnames);
+	addPtr(&ptrReg, &refnames, &FreeRefFile);
 
 	/* Initial values. */
 	cs1_sw.dqicorr = OMIT;
@@ -133,20 +142,24 @@ int main (int argc, char **argv) {
 
 	    if (strcmp (argv[i], "--version") == 0) {
 		PrVersion();
+		freeOnExit(&ptrReg);
 		exit (0);
 	    }
         if (!(strcmp(argv[i],"--gitinfo")))
         {
             printGitInfo();
+            freeOnExit(&ptrReg);
             exit(0);
         }
         if (!(strcmp(argv[i],"--help")))
         {
             printHelp();
+            freeOnExit(&ptrReg);
             exit(0);
         }
 	    if (strcmp (argv[i], "-r") == 0) {
 		PrFullVersion();
+		freeOnExit(&ptrReg);
 		exit (0);
 	    }
 	    if (strcmp (argv[i], "-dqi") == 0) {	/* turn on */
@@ -201,30 +214,34 @@ int main (int argc, char **argv) {
 		    } else {
 			printf ("ERROR:  Unrecognized option %s\n", argv[i]);
 			printSyntax();
+			freeOnExit(&ptrReg);
 			exit (1);
 		    }
 		}
 	    } else if (inlist[0] == '\0') {
-		free (inlist);
+	    freePtr(&ptrReg, inlist);
 		if ((inlist = calloc (strlen(argv[i])+1, sizeof(char)))
 			== NULL) {
 		    printf ("ERROR:  Out of memory.\n");
+		    freeOnExit(&ptrReg);
 		    exit (ERROR_RETURN);
 		}
 		strcpy (inlist, argv[i]);
 	    } else if (outlist[0] == '\0') {
-		free (outlist);
+	    freePtr(&ptrReg, outlist);
 		if ((outlist = calloc (strlen(argv[i])+1, sizeof(char)))
 			== NULL) {
 		    printf ("ERROR:  Out of memory.\n");
+		    freeOnExit(&ptrReg);
 		    exit (ERROR_RETURN);
 		}
 		strcpy (outlist, argv[i]);
 	    } else if (blevlist[0] == '\0') {
-		free (blevlist);
+	    freePtr(&ptrReg, blevlist);
 		if ((blevlist = calloc (strlen(argv[i])+1, sizeof(char)))
 			== NULL) {
 		    printf ("ERROR:  Out of memory.\n");
+		    freeOnExit(&ptrReg);
 		    exit (ERROR_RETURN);
 		}
 		strcpy (blevlist, argv[i]);
@@ -234,7 +251,7 @@ int main (int argc, char **argv) {
 	}
 	if (inlist[0] == '\0' || too_many) {
 	    printSyntax();
-	    FreeNames (inlist, outlist, blevlist, input, output, outblev);
+	    freeOnExit(&ptrReg);
 	    exit (ERROR_RETURN);
 	}
 
@@ -257,11 +274,17 @@ int main (int argc, char **argv) {
 
 	/* Expand the templates. */
 	i_imt = c_imtopen (inlist);
+	addPtr(&ptrReg, i_imt, &c_imtclose);
 	o_imt = c_imtopen (outlist);
+    addPtr(&ptrReg, o_imt, &c_imtclose);
 	b_imt = c_imtopen (blevlist);
+    addPtr(&ptrReg, b_imt, &c_imtclose);
 	n_in = c_imtlen (i_imt);
+    addPtr(&ptrReg, n_in, &c_imtclose);
 	n_out = c_imtlen (o_imt);
+    addPtr(&ptrReg, n_out, &c_imtclose);
 	n_blev = c_imtlen (b_imt);
+    addPtr(&ptrReg, n_blev, &c_imtclose);
 
 	/* The number of input and output files must be the same. */
 	status = 0;
@@ -270,7 +293,7 @@ int main (int argc, char **argv) {
 	if (CompareNumbers (n_in, n_blev, "outblev"))
 	    status = ERROR_RETURN;
 	if (status) {
-	    FreeNames (inlist, outlist, blevlist, input, output, outblev);
+	    freeOnExit(&ptrReg);
 	    exit (ERROR_RETURN);
 	}
 
@@ -303,12 +326,7 @@ int main (int argc, char **argv) {
 	    }
 	}
 
-	/* Close lists of file names, and free name buffers. */
-	c_imtclose (i_imt);
-	c_imtclose (o_imt);
-	c_imtclose (b_imt);
-	FreeRefFile (&refnames);
-	FreeNames (inlist, outlist, blevlist, input, output, outblev);
+    freeOnExit(&ptrReg);
 
 	if (status)
 	    exit (ERROR_RETURN);
@@ -336,21 +354,4 @@ static int CompareNumbers (int n_in, int n_out, char *str_out) {
 	}
 
 	return (0);
-}
-
-static void FreeNames (char *inlist, char *outlist, char *blevlist,
-		char *input, char *output, char *outblev) {
-
-	if (outblev != NULL)
-	    free (outblev);
-	if (output != NULL)
-	    free (output);
-	if (input != NULL)
-	    free (input);
-	if (blevlist != NULL)
-	    free (blevlist);
-	if (outlist != NULL)
-	    free (outlist);
-	if (inlist != NULL)
-	    free (inlist);
 }

pkg/stis/calstis/cs11/cs11.c
@@ -4,6 +4,7 @@
 # include <stdlib.h>		/* calloc */
 # include <string.h>
 
+#include "hstcal_memory.h"
 # include "c_iraf.h"		/* for c_irafinit */
 # include "ximio.h"
 
@@ -13,7 +14,6 @@
 # include "hstcalversion.h"
 
 static int CompareNumbers (int, char *, int, char *);
-static void FreeNames (char *, char *, char *, char *, char *, char *);
 static void printSyntax(void)
 {
     printf("syntax:  cs11.e [--help] [-t] [-v] [--version] [--gitinfo] wavecal science output\n");
@@ -67,16 +67,25 @@ int main (int argc, char **argv) {
 
 	c_irafinit (argc, argv);
 
+    PtrRegister ptrReg;
+    initPtrRegister(&ptrReg);
 	/* Allocate space for file names. */
 	wavlist = calloc (STIS_LINE+1, sizeof (char));
+	addPtr(&ptrReg, wavlist, &free);
 	scilist = calloc (STIS_LINE+1, sizeof (char));
+    addPtr(&ptrReg, scilist, &free);
 	outlist = calloc (STIS_LINE+1, sizeof (char));
+    addPtr(&ptrReg, outlist, &free);
 	inwav = calloc (STIS_LINE+1, sizeof (char));
+    addPtr(&ptrReg, inwav, &free);
 	insci = calloc (STIS_LINE+1, sizeof (char));
+    addPtr(&ptrReg, insci, &free);
 	output = calloc (STIS_LINE+1, sizeof (char));
-	if (wavlist == NULL || scilist == NULL || outlist == NULL ||
-		inwav == NULL || insci == NULL || output == NULL) {
+    addPtr(&ptrReg, output, &free);
+
+	if (!wavlist || !scilist || !outlist || !inwav || !insci || !output) {
 	    printf ("ERROR:  Can't even begin:  out of memory.\n");
+	    freeOnExit(&ptrReg);
 	    exit (ERROR_RETURN);
 	}
 
@@ -84,20 +93,24 @@ int main (int argc, char **argv) {
 	for (i = 1;  i < argc;  i++) {
 	    if (strcmp (argv[i], "--version") == 0) {
 		PrVersion();
+		freeOnExit(&ptrReg);
 		exit (0);
 	    }
         if (!(strcmp(argv[i],"--gitinfo")))
         {
             printGitInfo();
+            freeOnExit(&ptrReg);
             exit(0);
         }
         if (!(strcmp(argv[i],"--help")))
         {
             printHelp();
+            freeOnExit(&ptrReg);
             exit(0);
         }
 	    if (strcmp (argv[i], "-r") == 0) {
 		PrFullVersion();
+		freeOnExit(&ptrReg);
 		exit (0);
 	    }
 	    if (argv[i][0] == '-') {
@@ -109,6 +122,7 @@ int main (int argc, char **argv) {
 		    } else {
 			printf ("ERROR:  Unrecognized option %s\n", argv[i]);
 			printSyntax();
+			freeOnExit(&ptrReg);
 			exit (1);
 		    }
 		}
@@ -124,17 +138,24 @@ int main (int argc, char **argv) {
 	}
 	if (scilist[0] == '\0' || too_many) {
 	    printSyntax();
-	    FreeNames (wavlist, scilist, outlist, inwav, insci, output);
+	    freeOnExit(&ptrReg);
 	    exit (ERROR_RETURN);
 	}
 
 	/* Expand the templates. */
+
 	w_imt = c_imtopen (wavlist);
+    addPtr(&ptrReg, w_imt, &c_imtclose);
 	s_imt = c_imtopen (scilist);
+    addPtr(&ptrReg, s_imt, &c_imtclose);
 	o_imt = c_imtopen (outlist);
+    addPtr(&ptrReg, o_imt, &c_imtclose);
 	n_wav = c_imtlen (w_imt);
+    addPtr(&ptrReg, n_wav, &c_imtclose);
 	n_sci = c_imtlen (s_imt);
+    addPtr(&ptrReg, n_sci, &c_imtclose);
 	n_out = c_imtlen (o_imt);
+    addPtr(&ptrReg, n_out, &c_imtclose);
 
 	/* The number of wavecal and science files must be the same.
 	   The output may have been omitted, but if any output was
@@ -146,7 +167,7 @@ int main (int argc, char **argv) {
 	if (n_out > 0 && CompareNumbers (n_wav, "wavecal", n_out, "output"))
 	    status = ERROR_RETURN;
 	if (status) {
-	    FreeNames (wavlist, scilist, outlist, inwav, insci, output);
+	    freeOnExit(&ptrReg);
 	    exit (ERROR_RETURN);
 	}
 
@@ -174,11 +195,7 @@ int main (int argc, char **argv) {
 	    }
 	}
 
-	/* Close lists of file names, and free name buffers. */
-	c_imtclose (w_imt);
-	c_imtclose (s_imt);
-	c_imtclose (o_imt);
-	FreeNames (wavlist, scilist, outlist, inwav, insci, output);
+    freeOnExit(&ptrReg);
 
 	if (status)
 	    exit (ERROR_RETURN);
@@ -208,15 +225,4 @@ static int CompareNumbers (int n_in, char *str_in,
 	}
 
 	return (0);
-}
-
-static void FreeNames (char *wavlist, char *scilist, char *outlist,
-		char *inwav, char *insci, char *output) {
-
-	free (output);
-	free (insci);
-	free (inwav);
-	free (outlist);
-	free (scilist);
-	free (wavlist);
 }

pkg/acs/calacs/acs2d/main2d.c
@@ -8,6 +8,7 @@
 int status = 0;			/* zero is OK */
 
 # include <c_iraf.h>		/* for c_irafinit */
+#include "hstcal_memory.h"
 #include "hstcal.h"
 # include "ximio.h"
 # include "hstio.h"
@@ -20,7 +21,6 @@ int status = 0;			/* zero is OK */
 # include "hstcalversion.h"
 #include "trlbuf.h"
 
-static void FreeNames (char *, char *, char *, char *);
 static void printSyntax(void)
 {
     printf("syntax:  acs2d [--help] [-t] [-v] [-q] [--version] [--gitinfo] input output\n");
@@ -99,18 +99,25 @@ int main (int argc, char **argv) {
     c_irafinit (argc, argv);
 
     /* Allocate space for file names. */
+    PtrRegister ptrReg;
+    initPtrRegister(&ptrReg);
     inlist = calloc (CHAR_LINE_LENGTH+1, sizeof (char));
+    addPtr(&ptrReg, inlist, &free);
     outlist = calloc (CHAR_LINE_LENGTH+1, sizeof (char));
+    addPtr(&ptrReg, outlist, &free);
     input = calloc (CHAR_LINE_LENGTH+1, sizeof (char));
+    addPtr(&ptrReg, input, &free);
     output = calloc (CHAR_LINE_LENGTH+1, sizeof (char));
-    if (inlist == NULL || outlist == NULL ||
-        input == NULL || output == NULL) {
+    addPtr(&ptrReg, output, &free);
+    if (!inlist || !outlist || !input || !output) {
         printf ("Can't even begin; out of memory.\n");
+        freeOnExit(&ptrReg);
         exit (ERROR_RETURN);
     }
 
     /* Initialize the lists of reference file keywords and names. */
     InitRefFile (&refnames);
+    addPtr(&ptrReg, &refnames, &FreeRefFile);
 
     /* Initial values. */
     initSwitch (&acs2d_sw);
@@ -148,16 +155,19 @@ int main (int argc, char **argv) {
             if (!(strcmp(argv[i],"--version")))
             {
                 printf("%s\n",ACS_CAL_VER);
+                freeOnExit(&ptrReg);
                 exit(0);
             }
             if (!(strcmp(argv[i],"--gitinfo")))
             {
                 printGitInfo();
+                freeOnExit(&ptrReg);
                 exit(0);
             }
             if (!(strcmp(argv[i],"--help")))
             {
                 printHelp();
+                freeOnExit(&ptrReg);
                 exit(0);
             }
             for (j = 1;  argv[i][j] != '\0';  j++) {
@@ -170,7 +180,7 @@ int main (int argc, char **argv) {
                 } else {
                     printf ("Unrecognized option %s\n", argv[i]);
                     printSyntax();
-                    FreeNames (inlist, outlist, input, output);
+                    freeOnExit(&ptrReg);
                     exit (1);
                 }
             }
@@ -189,12 +199,13 @@ int main (int argc, char **argv) {
         printf ("       -dqi -glin -lflg -dark\n");
         printf ("       -flash -flat -shad -phot\n");
         */
-        FreeNames (inlist, outlist, input, output);
+        freeOnExit(&ptrReg);
         exit (ERROR_RETURN);
     }
 
     /* Initialize the structure for managing trailer file comments */
     InitTrlBuf ();
+    addPtr(&ptrReg, &trlbuf , &CloseTrlBuf);
     trlGitInfo();
 
     /* Copy command-line value for QUIET to structure */
@@ -214,7 +225,9 @@ int main (int argc, char **argv) {
 
     /* Expand the templates. */
     i_imt = c_imtopen (inlist);
+    addPtr(&ptrReg, i_imt, &c_imtclose);
     o_imt = c_imtopen (outlist);
+    addPtr(&ptrReg, o_imt, &c_imtclose);
     n_in = c_imtlen (i_imt);
     n_out = c_imtlen (o_imt);
 
@@ -223,8 +236,7 @@ int main (int argc, char **argv) {
         status = 1;
 
     if (status) {
-        FreeNames (inlist, outlist, input, output);
-        CloseTrlBuf(&trlbuf);
+        freeOnExit(&ptrReg);
         exit (ERROR_RETURN);
     }
 
@@ -284,24 +296,11 @@ int main (int argc, char **argv) {
         }
     }
 
-    /* Close lists of file names, and free name buffers
-       and trailer file buffer.
-    */
-    c_imtclose (i_imt);
-    c_imtclose (o_imt);
-    FreeRefFile (&refnames);
-    FreeNames (inlist, outlist, input, output);
-    CloseTrlBuf(&trlbuf);
+
+    freeOnExit(&ptrReg);
 
     if (status)
         exit (ERROR_RETURN);
     else
         exit (ACS_OK);
-}
-
-static void FreeNames (char *inlist, char *outlist, char *input, char *output) {
-	free (output);
-	free (input);
-	free (outlist);
-	free (inlist);
 }

pkg/acs/calacs/acs2d/wscript
@@ -5,7 +5,7 @@ def build(bld):
         name = 'acs2d.e',
         source = 'main2d.c',
         target = 'acs2d.e',
-        use = ['calacs', 'imphttab'] + bld.env.LOCAL_LIBS,
+        use = ['hstcallib', 'calacs', 'imphttab'] + bld.env.LOCAL_LIBS,
         lib = bld.env.EXTERNAL_LIBS,
         libpath = bld.env.LIBPATH,
         install_path = '${PREFIX}/bin'

pkg/acs/calacs/acsccd/mainccd.c
@@ -8,6 +8,7 @@
 int status = 0;			/* zero is OK */
 
 # include <c_iraf.h>		/* for c_irafinit */
+#include "hstcal_memory.h"
 #include "hstcal.h"
 # include "ximio.h"
 # include "hstio.h"
@@ -24,7 +25,6 @@ int status = 0;			/* zero is OK */
 char MsgText[MSG_BUFF_LENGTH]; // Global char auto initialized to '\0'
 struct TrlBuf trlbuf = { 0 };
 
-static void FreeNames (char *, char *, char *, char *);
 static void printSyntax(void)
 {
     printf ("syntax:  acsccd [--help] [-t] [-v] [-q] [--version] [--gitinfo] input output\n");
@@ -98,15 +98,21 @@ int main (int argc, char **argv) {
 
     c_irafinit (argc, argv);
 
+    PtrRegister ptrReg;
+    initPtrRegister(&ptrReg);
     /* Allocate space for file names. */
     inlist = calloc (CHAR_LINE_LENGTH+1, sizeof (char));
+    addPtr(&ptrReg, inlist, &free);
     outlist = calloc (CHAR_LINE_LENGTH+1, sizeof (char));
+    addPtr(&ptrReg, outlist, &free);
     input = calloc (CHAR_LINE_LENGTH+1, sizeof (char));
+    addPtr(&ptrReg, input, &free);
     output = calloc (CHAR_LINE_LENGTH+1, sizeof (char));
+    addPtr(&ptrReg, output, &free);
 
-    if (inlist == NULL || outlist == NULL ||
-        input == NULL || output == NULL) {
+    if (!inlist || !outlist|| !input || !output) {
         printf ("Can't even begin; out of memory.\n");
+        freeOnExit(&ptrReg);
         exit (ERROR_RETURN);
     }
     inlist[0] = '\0';
@@ -116,6 +122,7 @@ int main (int argc, char **argv) {
 
     /* Initialize the lists of reference file keywords and names. */
     InitRefFile (&refnames);
+    addPtr(&ptrReg, &refnames, &FreeRefFile);
 
     /* Initial values. */
     initSwitch (&ccd_sw);
@@ -141,16 +148,19 @@ int main (int argc, char **argv) {
             if (!(strcmp(argv[i],"--version")))
             {
                 printf("%s\n",ACS_CAL_VER);
+                freeOnExit(&ptrReg);
                 exit(0);
             }
             if (!(strcmp(argv[i],"--gitinfo")))
             {
                 printGitInfo();
+                freeOnExit(&ptrReg);
                 exit(0);
             }
             if (!(strcmp(argv[i],"--help")))
             {
                 printHelp();
+                freeOnExit(&ptrReg);
                 exit(0);
             }
             for (j = 1;  argv[i][j] != '\0';  j++) {
@@ -163,7 +173,7 @@ int main (int argc, char **argv) {
                 } else {
                     printf (MsgText, "Unrecognized option %s\n", argv[i]);
                     printSyntax();
-                    FreeNames (inlist, outlist, input, output);
+                    freeOnExit(&ptrReg);
                     exit (1);
                 }
             }
@@ -181,11 +191,12 @@ int main (int argc, char **argv) {
         printf ("  command-line switches:\n");
         printf ("       -dqi -atod -blev -bias\n");
         */
-        FreeNames (inlist, outlist, input, output);
+        freeOnExit(&ptrReg);
         exit (ERROR_RETURN);
     }
     /* Initialize the structure for managing trailer file comments */
     InitTrlBuf ();
+    addPtr(&ptrReg, &trlbuf, &CloseTrlBuf);
     trlGitInfo();
 
     /* Copy command-line value for QUIET to structure */
@@ -202,7 +213,9 @@ int main (int argc, char **argv) {
 
     /* Expand the templates. */
     i_imt = c_imtopen (inlist);
+    addPtr(&ptrReg, i_imt, &c_imtclose);
     o_imt = c_imtopen (outlist);
+    addPtr(&ptrReg, o_imt, &c_imtclose);
     n_in = c_imtlen (i_imt);
     n_out = c_imtlen (o_imt);
 
@@ -210,8 +223,7 @@ int main (int argc, char **argv) {
     if (CompareNumbers (n_in, n_out, "output"))
         status = 1;
     if (status) {
-        FreeNames (inlist, outlist, input, output);
-        CloseTrlBuf(&trlbuf);
+        freeOnExit(&ptrReg);
         exit (ERROR_RETURN);
     }
 
@@ -251,23 +263,11 @@ int main (int argc, char **argv) {
         }
     }
 
-    /* Close lists of file names, and free name buffers. */
-    c_imtclose (i_imt);
-    c_imtclose (o_imt);
-    FreeRefFile (&refnames);
-    FreeNames (inlist, outlist, input, output);
-    CloseTrlBuf(&trlbuf);
+
+    freeOnExit(&ptrReg);
 
     if (status)
         exit (ERROR_RETURN);
     else
         exit (0);
-}
-
-
-static void FreeNames (char *inlist, char *outlist, char *input, char *output) {
-    free (output);
-    free (input);
-    free (outlist);
-    free (inlist);
 }

pkg/acs/calacs/acsccd/wscript
@@ -4,7 +4,7 @@ def build(bld):
     t = bld.program(
         source = 'mainccd.c',
         target = 'acsccd.e',
-        use = ['calacs', 'imphttab'] + bld.env.LOCAL_LIBS,
+        use = ['hstcallib', 'calacs', 'imphttab'] + bld.env.LOCAL_LIBS,
         lib = bld.env.EXTERNAL_LIBS,
         libpath = bld.env.LIBPATH,
         install_path = '${PREFIX}/bin'

pkg/acs/calacs/acscte/maincte.c
@@ -8,6 +8,7 @@
 int status = 0;			/* zero is OK */
 
 # include <c_iraf.h>		/* for c_irafinit */
+#include "hstcal_memory.h"
 #include "hstcal.h"
 # include "ximio.h"
 # include "hstio.h"
@@ -24,7 +25,6 @@ int status = 0;			/* zero is OK */
 #  include <omp.h>
 # endif
 
-static void FreeNames (char *, char *, char *, char *);
 struct TrlBuf trlbuf = { 0 };
 
 /* Standard string buffer for use in messages */
@@ -98,15 +98,21 @@ int main (int argc, char **argv) {
 
     c_irafinit (argc, argv);
 
+    PtrRegister ptrReg;
+    initPtrRegister(&ptrReg);
     /* Allocate space for file names. */
     inlist = calloc (CHAR_LINE_LENGTH+1, sizeof (char));
+    addPtr(&ptrReg, inlist, &free);
     outlist = calloc (CHAR_LINE_LENGTH+1, sizeof (char));
+    addPtr(&ptrReg, outlist, &free);
     input = calloc (CHAR_LINE_LENGTH+1, sizeof (char));
+    addPtr(&ptrReg, input, &free);
     output = calloc (CHAR_LINE_LENGTH+1, sizeof (char));
+    addPtr(&ptrReg, output, &free);
 
-    if (inlist == NULL || outlist == NULL ||
-        input == NULL || output == NULL) {
+    if (!inlist || !outlist || !input || !output) {
         printf ("Can't even begin; out of memory.\n");
+        freeOnExit(&ptrReg);
         exit (ERROR_RETURN);
     }
     inlist[0] = '\0';
@@ -116,6 +122,7 @@ int main (int argc, char **argv) {
 
     /* Initialize the lists of reference file keywords and names. */
     InitRefFile (&refnames);
+    addPtr(&ptrReg, &refnames, &FreeRefFile);
 
     /* Initial values. */
     initSwitch (&ccd_sw);
@@ -126,16 +133,19 @@ int main (int argc, char **argv) {
             if (!(strcmp(argv[i],"--version")))
             {
                 printf("%s\n",ACS_CAL_VER);
+                freeOnExit(&ptrReg);
                 exit(0);
             }
             if (!(strcmp(argv[i],"--gitinfo")))
             {
                 printGitInfo();
+                freeOnExit(&ptrReg);
                 exit(0);
             }
             if (!(strcmp(argv[i],"--help")))
             {
                 printHelp();
+                freeOnExit(&ptrReg);
                 exit(0);
             }
             if (strncmp(argv[i], "--ctegen", 8) == 0)
@@ -143,6 +153,7 @@ int main (int argc, char **argv) {
                 if (i + 1 > argc - 1)
                 {
                     printf("ERROR: --ctegen - CTE algorithm generation not specified.\n");
+                    freeOnExit(&ptrReg);
                     exit(1);
                 }
                 ++i;
@@ -150,6 +161,7 @@ int main (int argc, char **argv) {
                 if (cteAlgorithmGen != 1 && cteAlgorithmGen != 2)
                 {
                     printf("ERROR: --ctegen - value out of range. Please specify either generation 1 or 2.\n");
+                    freeOnExit(&ptrReg);
                     exit(1);
                 }
                 continue;
@@ -159,6 +171,7 @@ int main (int argc, char **argv) {
                 if (i + 1 > argc - 1)
                 {
                     printf("ERROR: --nthreads - number of threads not specified\n");
+                    freeOnExit(&ptrReg);
                     exit(1);
                 }
                 ++i;
@@ -176,6 +189,7 @@ int main (int argc, char **argv) {
                 if (i + 1 > argc - 1)
                 {
                     printf("ERROR: --pctetab - no file specified\n");
+                    freeOnExit(&ptrReg);
                     exit(1);
                 }
                 ++i;
@@ -188,6 +202,7 @@ int main (int argc, char **argv) {
                 {
                     printf ("Unrecognized option %s\n", argv[i]);
                     printSyntax();
+                    freeOnExit(&ptrReg);
                     exit (ERROR_RETURN);
                 }
                 for (j = 1;  argv[i][j] != '\0';  j++) {
@@ -216,11 +231,12 @@ int main (int argc, char **argv) {
     }
     if (inlist[0] == '\0' || too_many) {
         printSyntax();
-        FreeNames (inlist, outlist, input, output);
+        freeOnExit(&ptrReg);
         exit (ERROR_RETURN);
     }
     /* Initialize the structure for managing trailer file comments */
     InitTrlBuf ();
+    addPtr(&ptrReg, &trlbuf, &CloseTrlBuf);
     trlGitInfo();
 
     /* Copy command-line value for QUIET to structure */
@@ -278,7 +294,9 @@ int main (int argc, char **argv) {
 
     /* Expand the templates. */
     i_imt = c_imtopen (inlist);
+    addPtr(&ptrReg, i_imt, &c_imtclose);
     o_imt = c_imtopen (outlist);
+    addPtr(&ptrReg, o_imt, &c_imtclose);
     n_in = c_imtlen (i_imt);
     n_out = c_imtlen (o_imt);
 
@@ -286,8 +304,7 @@ int main (int argc, char **argv) {
     if (CompareNumbers (n_in, n_out, "output"))
         status = 1;
     if (status) {
-        FreeNames (inlist, outlist, input, output);
-        CloseTrlBuf(&trlbuf);
+        freeOnExit(&ptrReg);
         exit (ERROR_RETURN);
     }
 
@@ -341,23 +358,11 @@ int main (int argc, char **argv) {
         }
     }
 
-    /* Close lists of file names, and free name buffers. */
-    c_imtclose (i_imt);
-    c_imtclose (o_imt);
-    FreeRefFile (&refnames);
-    FreeNames (inlist, outlist, input, output);
-    CloseTrlBuf(&trlbuf);
+
+    freeOnExit(&ptrReg);
 
     if (status)
         exit (ERROR_RETURN);
     else
         exit (0);
-}
-
-
-static void FreeNames (char *inlist, char *outlist, char *input, char *output) {
-    free (output);
-    free (input);
-    free (outlist);
-    free (inlist);
 }

pkg/acs/calacs/acscte/wscript
@@ -13,8 +13,10 @@ def build(bld):
                   'maincte.c'
                   ],
         target = 'acscte.e',
-        includes = ['.', '../../../../ctegen2'],
-        use = ['calacs', 'imphttab', 'ctegen2', 'hstcallib'] + bld.env.LOCAL_LIBS,
+        includes = ['.',
+                    '../../../../ctegen2',
+                    '../../../../include'],
+        use = ['hstcallib', 'calacs', 'imphttab', 'ctegen2'] + bld.env.LOCAL_LIBS,
         lib = bld.env.EXTERNAL_LIBS,
         libpath = bld.env.LIBPATH,
         install_path = '${PREFIX}/bin'

commit 8b1991b46e7fc87cb347dee15272f80c67d93e11

Remove incorrect non-ptr registrations
Fixes #280

Regression introduced from #275

Signed-off-by: James Noss <jnoss@stsci.edu>

pkg/stis/calstis/cs1/cs1.c
@@ -280,11 +280,8 @@ int main (int argc, char **argv) {
 	b_imt = c_imtopen (blevlist);
     addPtr(&ptrReg, b_imt, &c_imtclose);
 	n_in = c_imtlen (i_imt);
-    addPtr(&ptrReg, n_in, &c_imtclose);
 	n_out = c_imtlen (o_imt);
-    addPtr(&ptrReg, n_out, &c_imtclose);
 	n_blev = c_imtlen (b_imt);
-    addPtr(&ptrReg, n_blev, &c_imtclose);
 
 	/* The number of input and output files must be the same. */
 	status = 0;

pkg/stis/calstis/cs11/cs11.c
@@ -151,11 +151,8 @@ int main (int argc, char **argv) {
 	o_imt = c_imtopen (outlist);
     addPtr(&ptrReg, o_imt, &c_imtclose);
 	n_wav = c_imtlen (w_imt);
-    addPtr(&ptrReg, n_wav, &c_imtclose);
 	n_sci = c_imtlen (s_imt);
-    addPtr(&ptrReg, n_sci, &c_imtclose);
 	n_out = c_imtlen (o_imt);
-    addPtr(&ptrReg, n_out, &c_imtclose);
 
 	/* The number of wavecal and science files must be the same.
 	   The output may have been omitted, but if any output was