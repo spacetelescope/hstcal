db2aa80afab56ded75393fce7ad6937ce3a74f89
Warren Hack <hack@stsci.edu>
2019-10-31 14:54:36 -0400

    Update make file

--- docs/make.bat
+++ docs/make.bat
@@ -74,9 +74,9 @@ if "%1" == "qthelp" (
 	echo.
 	echo.Build finished; now you can run "qcollectiongenerator" with the ^
 .qhcp project file in %BUILDDIR%/qthelp, like this:
-	echo.^> qcollectiongenerator %BUILDDIR%\qthelp\drizzlepac.qhcp
+	echo.^> qcollectiongenerator %BUILDDIR%\qthelp\hstcal.qhcp
 	echo.To view the help file:
-	echo.^> assistant -collectionFile %BUILDDIR%\qthelp\drizzlepac.ghc
+	echo.^> assistant -collectionFile %BUILDDIR%\qthelp\hstcal.ghc
 	goto end
 )
 
