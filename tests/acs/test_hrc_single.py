6c72a3d43fa11f795b663c65dd5edbcce32e5d64
Michele De La Pena <mdelapena@stsci.edu>
2020-05-07 16:41:25 -0400

    Removed redundant test as this particular RAW is used in two tests (processed as a singleton and processed as part of an association).  This causes confusion when generating updated truth files after a reference file is updated corresponding with a software change.

--- tests/acs/test_hrc_single.py
+++ tests/acs/test_hrc_single.py
@@ -38,10 +38,9 @@ class TestSingle(BaseACS):
 
     # NOTE:
     # j8bt02loq = was hrc_single1
-    # j8cd02tyq = was hrc_single2
-    # j8bt02lo2 = was hrc_single3 with FLSHCORR
+    # j8bt02lo2 = was hrc_single1 with FLSHCORR
     @pytest.mark.parametrize(
-        'rootname', ['j8bt02loq', 'j8cd02tyq', 'j8bt02lo2'])
+        'rootname', ['j8bt02loq', 'j8bt02lo2'])
     def test_fullframe_single(self, rootname):
         self._single_raw_calib(rootname)
 
