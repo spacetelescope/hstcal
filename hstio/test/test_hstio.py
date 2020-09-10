f73d218cf8499eb540b4d8d85ac9640937e6c43b
Sean Lockwood <lockwood@stsci.edu>
2020-09-10 10:50:39 -0400

    Changed pyfits-->astropy; fixed prints; closing HDUs

--- hstio/test/test_hstio.py
+++ hstio/test/test_hstio.py
@@ -1,7 +1,7 @@
 # Run with nosetests
 import os
 import platform
-import pyfits
+from astropy.io import fits
 import shutil
 
 path = os.path.dirname(__file__)
@@ -15,10 +15,10 @@ def test_hstio_resize_hdu():
     assert os.system("%s %s" % (join(bin_path, "test_hstio_resize_hdu"),
                                 join(path, "test_hstio_resize_hdu_tmp.fits"))) == 0
 
-    hdulist = pyfits.open(join(path, "test_hstio_resize_hdu_tmp.fits"))
-    assert hdulist[1].data is not None
-    print hdulist[1].data[34, 12]
-    assert hdulist[1].data[34,12] == 43
+    with fits.open(join(path, "test_hstio_resize_hdu_tmp.fits")) as hdulist:
+        assert hdulist[1].data is not None
+        print (hdulist[1].data[34, 12])
+        assert hdulist[1].data[34,12] == 43
 
 def test_hstio_resize_hdu2():
     shutil.copyfile(join(path, "test_hstio_resize_hdu.fits"),
@@ -27,9 +27,9 @@ def test_hstio_resize_hdu2():
     assert os.system("%s %s" % (join(bin_path, "test_hstio_resize_hdu2"),
                                 join(path, "test_hstio_resize_hdu_tmp.fits"))) == 0
 
-    hdulist = pyfits.open(join(path, "test_hstio_resize_hdu_tmp.fits"))
-    assert hdulist[1].data is not None
-    assert hdulist[1].data[1000,50] == 50
-    print hdulist[1].data[42, 42]
-    assert hdulist[1].data[42, 42] == 42
+    with fits.open(join(path, "test_hstio_resize_hdu_tmp.fits")) as hdulist:
+        assert hdulist[1].data is not None
+        assert hdulist[1].data[1000,50] == 50
+        print (hdulist[1].data[42, 42])
+        assert hdulist[1].data[42, 42] == 42
 
