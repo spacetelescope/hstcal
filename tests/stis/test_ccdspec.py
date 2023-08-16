b3f5789e39b83f590085da35e1a3c6cacaf981b2
Robert Jedrzejewski <rij@stsci.edu>
2023-08-16 12:03:12 -0400

    Unskip existing calstis regression test, having updated truth files on artifactory Add new test for CCD spectroscopy

--- tests/stis/test_ccdspec.py
+++ tests/stis/test_ccdspec.py
@@ -7,22 +7,24 @@ from ..helpers import BaseSTIS
 
 class TestLev1FUVSpec(BaseSTIS):
     """Level 1 FUV-MAMA spectroscopy."""
-    detector = 'fuv-mama'
+    detector = 'ccd'
 
     def test_lev1(self):
         """This was ``calstis_lev1_FUVspec``."""
-        raw_file = 'o5cl02040_raw.fits'
-        wav_file = 'o5cl02040_wav.fits'
+        raw_file = 'o55108010_raw.fits'
+        wav_file = 'o55108010_wav.fits'
 
         # Prepare input files.
         self.get_input_file(raw_file)
-        get_bigdata('scsb-hstcal', self.env, 'stis', 'fuv-mama', 'input',
+        get_bigdata('scsb-hstcal', self.env, 'stis', 'ccd', 'input',
                     wav_file)
 
         # Run CALSTIS (equivalent to stistools.calstis.calstis)
         subprocess.call(['cs0.e', raw_file, '-v'])
 
         # Compare results
-        outputs = [('o5cl02040_flt.fits', 'calstis_lev1_FUVspec_flt.fits'),
-                   ('o5cl02040_x1d.fits', 'calstis_lev1_FUVspec_x1d.fits')]
+        outputs = [('o55108010_flt.fits', 'calstis_ccdspec_flt.fits'),
+                   ('o55108010_sx1.fits', 'calstis_ccdspec_sx1.fits'),
+                   ('o55108010_crj.fits', 'calstis_ccdspec_crj.fits'),
+                   ('o55108010_sx2.fits', 'calstis_ccdspec_sx2.fits')]
         self.compare_outputs(outputs)
