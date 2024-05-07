f0b705d7f54817f15ac67126a8fbf07952df871e
Michele De La Pena <mdelapena@stsci.edu>
2024-05-07 13:50:22 -0400

    Update date associated with version of code.

--- pkg/acs/include/acsversion.h
+++ pkg/acs/include/acsversion.h
@@ -2,7 +2,7 @@
 #define INCL_ACSVERSION_H
 
 /* This string is written to the output primary header as CAL_VER. */
-#define ACS_CAL_VER "10.4.0 (14-Sept-2023)"
+#define ACS_CAL_VER "10.4.0 (07-May-2024)"
 #define ACS_CAL_VER_NUM "10.4.0"
 
 /* This Generation of the CTE algorithm is obsolete.  These strings
