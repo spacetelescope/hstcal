36290b5edd64299f72c43931ee9955d902b152ac
mdlpstsci <mdelapena@stsci.edu>
2024-06-04 11:48:54 -0400

    This commit was pushed to main pre-maturely, and the repo is now (#614)
    being prepared for a build.
    Revert "WIP: HLA-1036: An 'MJD#' value is added to the PHOTMODE keyword in the PHDU (#612)"
    
    This reverts commit 0276e46c51512eb4969a86004642c8bb2add703e.

--- pkg/wfc3/include/wf3version.h
+++ pkg/wfc3/include/wf3version.h
@@ -4,7 +4,7 @@
 /* This string is written to the output primary header as CAL_VER. */
 
 
-# define WF3_CAL_VER "3.7.2 (Apr-15-2024)"
-# define WF3_CAL_VER_NUM "3.7.2"
+# define WF3_CAL_VER "3.7.1 (Oct-18-2023)"
+# define WF3_CAL_VER_NUM "3.7.1"
 
 #endif /* INCL_WF3VERSION_H */
