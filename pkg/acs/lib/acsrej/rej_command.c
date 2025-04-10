088700de31406954e543e5dae74615c431c83b19
Joseph Hunkeler <jhunkeler@gmail.com>
2025-04-10 16:18:58 -0400

    reg_command.c: Fix the argument parser
    * The usage statement has been moved to its own function and is called when an appropriate input error occurs.
    * Enable arguments common to other programs (-version, -gitinfo, -help) despite the use of "-long-option" with a single dash.
    * The double-dash argument format will work for the arguments listed above (--version, --gitinfo, --help)
    * Positional arguments "input" and "output" are parsed in the loop instead of individually
    * Executing the program with too many/few arguments will throw an error.

--- pkg/acs/lib/acsrej/rej_command.c
+++ pkg/acs/lib/acsrej/rej_command.c
@@ -6,14 +6,50 @@
 
 # include "acs.h"
 # include "acsrej.h"
+# include "acsversion.h"
 # include "hstcalerr.h"
 
 void rej_reset (clpar *, int []);
+static void usage(char *);
 static int syntax_error (char *);
 static int getArgS (char **, int, int *, short *);
 static int getArgR (char **, int, int *, float *);
 static int getArgT (char **, int, int *, char *);
 
+static void usage(char *program_name) {
+    char *name = strrchr(program_name, '/');
+    if (name) {
+        name++;
+    } else {
+        name = program_name;
+    }
+
+    printf("%s input output [-h] [-t] [-v]\n", name);
+    printf("                [-version] [-gitinfo]\n");
+    printf("                [-shadcorr] [-crmask] [-readnoise_only]\n");
+    printf("                [-table <filename>] [-scale #]\n");
+    printf("                [-init (med|min)] [-sky (none|mode)]\n");
+    printf("                [-sigmas #] [-radius #] [-thresh #]\n");
+    printf("                [-pdq #]\n\n");
+    printf("input             comma-delimited string (e.g., filename or filename1,filename2,filename3)\n");
+    printf("output            string\n");
+    printf("-h                show this help message\n");
+    printf("-t                print timestamps\n");
+    printf("-v                turn on verbose mode\n");
+    printf("-version          display program version\n");
+    printf("-gitinfo          display git version information\n");
+    printf("-shadcorr         turn on shutter shading correction (intended for CCD images only)\n");
+    printf("-crmask           flag CR-rejected pixels in the input files\n");
+    printf("-readnoise_only   use read noise and not Poission noise to create ERR array (intended for BIAS images)\n");
+    printf("-table filename   cosmic ray rejection table to use for default parameter values\n");
+    printf("-scale #          multiplicative factor (in percent) applied to noise\n");
+    printf("-init med|min     scheme for computing initial guess image (median or minimum)\n");
+    printf("-sky none|mode    scheme for computing sky levels to be subtracted (none:0.0 or mode:most frequently occurring value)\n");
+    printf("-sigmas #[,#...]  cosmic ray rejection thresholds, no. of thresholds are the no. of iterations\n");
+    printf("-radius #         radius (in pixels) to propagate the cosmic ray\n");
+    printf("-thresh #         cosmic ray rejection propagation threshold\n");
+    printf("-pdq #            data quality flag used for cosmic ray rejection\n\n");
+}
 /*
    Processes command-line parameters for AcsRej.
 
@@ -38,152 +74,163 @@ int newpar[];       o: array of parameters set by the user
 
     extern int status;
 
-    int ctoken;     /* current command-line token being processed */
-
     /* reset the parameters */
     rej_reset (par, newpar);
 
     /* not enough arguments
         List all options for user
     */
-    if (argc < 3) {
-        syntax_error ("acsrej.e input output [-t] [-v]");
-        printf("                    [-shadcorr] [-crmask] [-readnoise_only] \n");
-        printf("                    [-table <filename>] [-scale #]\n");
-        printf("                    [-init (med|min)] [-sky (none|mode)]\n");
-        printf("                    [-sigmas #] [-radius #] [-thresh #]\n");
-        printf("                    [-pdq #]\n\n");
-		printf("input             comma-delimited string (e.g., filename or filename1,filename2,filename3)\n");
-		printf("output            string\n");
-		printf("-t                print timestamps\n");
-		printf("-v                turn on verbose mode\n");
-		printf("-shadcorr         turn on shutter shading correction (intended for CCD images only)\n");
-		printf("-crmask           flag CR-rejected pixels in the input files\n");
-		printf("-readnoise_only   use read noise and not Poission noise to create ERR array (intended for BIAS images)\n");
-		printf("-table filename   cosmic ray rejection table to use for default parameter values\n");
-		printf("-scale #          multiplicative factor (in percent) applied to noise\n");
-		printf("-init med|min     scheme for computing initial guess image (median or minimum)\n");
-		printf("-sky none|mode    scheme for computing sky levels to be subtracted (none:0.0 or mode:most frequently occurring value)\n");
-		printf("-sigmas #[,#...]  cosmic ray rejection thresholds, no. of thresholds are the no. of iterations\n");
-		printf("-radius #         radius (in pixels) to propagate the cosmic ray\n");
-		printf("-thresh #         cosmic ray rejection propagation threshold\n");
-		printf("-pdq #            data quality flag used for cosmic ray rejection\n\n");
-        return(status);
+    if (argc < 2) {
+        fprintf(stderr, "Not enough arguments\n");
+	    usage(argv[0]);
+        return(status = INVALID_VALUE);
     }
 
-    /* Get names of input and output files. These are mandatory.
-      Input list is also limited by tables/c_imt.c */
-    if ((*input = calloc(strlen(argv[1]) + 1, sizeof(char))) == NULL) {
-        printf ("Can't even begin; out of memory.\n");
-        return (status = OUT_OF_MEMORY);
-    }
-    strcpy (*input, argv[1]);
-    strcpy (output, argv[2]);
+    // To store "input" and "output" strings
+    char *posargs[2];
+    const int posargs_max = sizeof(posargs) / sizeof(*posargs);
+    int posargs_count = 0;
 
     /* Scan remaining parameters. */
-    if (argc > 3) {
-        ctoken = 3;
-        while (ctoken < argc) {
-
-            /* switch must begin with a "-" */
-            if (argv[ctoken][0] != '-') {
-                return(syntax_error (argv[ctoken]));
-            } else {
-
+    for (int ctoken = 1; ctoken < argc; ctoken++) {
+        /* switch must begin with a "-" */
+        if (argv[ctoken][0] != '-') {
+            if (posargs_count >= posargs_max) {
+                posargs_count++;
+                continue;
+            }
+            posargs[posargs_count] = strdup(argv[ctoken]);
+            if (!posargs[posargs_count]) {
+                fprintf (stderr, "Can't even begin; out of memory.\n");
+                return (status = OUT_OF_MEMORY);
+            }
+            posargs_count++;
+        } else {
             /* These do not require additional arguments. */
+            if (strcmp("h", argv[ctoken] + 1) == 0 || strcmp("help", argv[ctoken] + 1) == 0 || strcmp("-help", argv[ctoken] + 1) == 0) {
+                usage(argv[0]);
+                exit(0);
+            }
 
-            if (strcmp("t", argv[ctoken]+1) == 0) {
+            if (strcmp("t", argv[ctoken] + 1) == 0) {
                 par->printtime = 1;
                 ctoken++;
-
-            } else if (strcmp("v", argv[ctoken]+1) == 0) {
+            } else if (strcmp("v", argv[ctoken] + 1) == 0) {
                 par->verbose = 1;
                 ctoken++;
-
-            } else if (strcmp("shadcorr", argv[ctoken]+1) == 0) {
+            } else if (strcmp("version", argv[ctoken] + 1) == 0 || strcmp("-version", argv[ctoken] + 1) == 0) {
+                // forced acceptance: -version and --version
+                puts(ACS_CAL_VER);
+                exit(0);
+            } else if (strcmp("gitinfo", argv[ctoken] + 1) == 0 || strcmp("-gitinfo", argv[ctoken] + 1) == 0) {
+                // forced acceptance: -gitinfo and --gitinfo
+                trlGitInfo();
+                exit(0);
+            } else if (strcmp("shadcorr", argv[ctoken] + 1) == 0) {
                 par->shadcorr = 1;
                 ctoken++;
 
-            // This option/variable was previously named "newbias", and it has been renamed
-            // to "readnoise_only" which is more informative.  The variable controls
-            // the type of noise used in the computation of the error (ERR) array.  In particular,
-            // this variable is intended to control the noise used when processing BIAS data.  When
-            // using CALACS, readnoise_only will only be set to "1" (true) for BIAS data.  When using
-            // ACSREJ as a standalone utility, the readnoise_only can be set to "1" for any data.
-            } else if (strcmp("newbias", argv[ctoken]+1) == 0) {
-                printf ("\n***************************************************************************************\n");
-				printf ("     As of HSTCAL Version 2.0.0 (CALACS Version 10.0.0 HSTDP 2018_1) the 'newbias'\n ");
-                printf ("    option for ACSREJ has been renamed 'readnoise_only' to clarify functionality.\n ");
-                printf ("    Please update all programs which invoke ACSREJ with this option.\n");
-                printf ("***************************************************************************************\n\n");
+                // This option/variable was previously named "newbias", and it has been renamed
+                // to "readnoise_only" which is more informative.  The variable controls
+                // the type of noise used in the computation of the error (ERR) array.  In particular,
+                // this variable is intended to control the noise used when processing BIAS data.  When
+                // using CALACS, readnoise_only will only be set to "1" (true) for BIAS data.  When using
+                // ACSREJ as a standalone utility, the readnoise_only can be set to "1" for any data.
+            } else if (strcmp("newbias", argv[ctoken] + 1) == 0) {
+                printf("\n***************************************************************************************\n");
+                printf("     As of HSTCAL Version 2.0.0 (CALACS Version 10.0.0 HSTDP 2018_1) the 'newbias'\n ");
+                printf("    option for ACSREJ has been renamed 'readnoise_only' to clarify functionality.\n ");
+                printf("    Please update all programs which invoke ACSREJ with this option.\n");
+                printf("***************************************************************************************\n\n");
                 return (status = INVALID_VALUE);
 
-            } else if (strcmp("readnoise_only", argv[ctoken]+1) == 0) {
+            } else if (strcmp("readnoise_only", argv[ctoken] + 1) == 0) {
                 par->readnoise_only = 1;
                 ctoken++;
 
-            } else if (strcmp("crmask", argv[ctoken]+1) == 0) {
+            } else if (strcmp("crmask", argv[ctoken] + 1) == 0) {
                 par->mask = 1;
                 newpar[CRMASK] = 1;
                 newpar[TOTAL]++;
                 ctoken++;
 
-            /* These require one additional argument, which is
-               handled by the getArg functions.
-            */
+                /* These require one additional argument, which is
+                   handled by the getArg functions.
+                */
 
-            } else if (strcmp("table", argv[ctoken]+1) == 0) {
-                if (getArgT (argv, argc, &ctoken, par->tbname))
+            } else if (strcmp("table", argv[ctoken] + 1) == 0) {
+                if (getArgT(argv, argc, &ctoken, par->tbname))
                     return (status = INVALID_VALUE);
 
-            } else if (strcmp("scale", argv[ctoken]+1) == 0) {
+            } else if (strcmp("scale", argv[ctoken] + 1) == 0) {
                 newpar[TOTAL]++;
                 newpar[SCALENSE] = 1;
-                if (getArgR (argv, argc, &ctoken, &par->scalense))
+                if (getArgR(argv, argc, &ctoken, &par->scalense))
                     return (status = INVALID_VALUE);
 
-            } else if (strcmp("init", argv[ctoken]+1) == 0) {
+            } else if (strcmp("init", argv[ctoken] + 1) == 0) {
                 newpar[TOTAL]++;
                 newpar[INITGUES] = 1;
-                if (getArgT (argv, argc, &ctoken, par->initgues))
+                if (getArgT(argv, argc, &ctoken, par->initgues))
                     return (status = INVALID_VALUE);
 
-            } else if (strcmp("sky", argv[ctoken]+1) == 0) {
+            } else if (strcmp("sky", argv[ctoken] + 1) == 0) {
                 newpar[TOTAL]++;
                 newpar[SKYSUB] = 1;
-                if (getArgT (argv, argc, &ctoken, par->sky))
+                if (getArgT(argv, argc, &ctoken, par->sky))
                     return (status = INVALID_VALUE);
 
-            } else if (strcmp("sigmas", argv[ctoken]+1) == 0) {
+            } else if (strcmp("sigmas", argv[ctoken] + 1) == 0) {
                 newpar[TOTAL]++;
                 newpar[CRSIGMAS] = 1;
-                if (getArgT (argv, argc, &ctoken, par->sigmas))
+                if (getArgT(argv, argc, &ctoken, par->sigmas))
                     return (status = INVALID_VALUE);
 
-            } else if (strcmp("radius", argv[ctoken]+1) == 0) {
+            } else if (strcmp("radius", argv[ctoken] + 1) == 0) {
                 newpar[TOTAL]++;
                 newpar[CRRADIUS] = 1;
-                if (getArgR (argv, argc, &ctoken, &par->radius))
+                if (getArgR(argv, argc, &ctoken, &par->radius))
                     return (status = INVALID_VALUE);
 
-            } else if (strcmp("thresh", argv[ctoken]+1) == 0) {
+            } else if (strcmp("thresh", argv[ctoken] + 1) == 0) {
                 newpar[TOTAL]++;
                 newpar[CRTHRESH] = 1;
-                if (getArgR (argv, argc, &ctoken, &par->thresh))
+                if (getArgR(argv, argc, &ctoken, &par->thresh))
                     return (status = INVALID_VALUE);
 
-            } else if (strcmp("pdq", argv[ctoken]+1) == 0) {
+            } else if (strcmp("pdq", argv[ctoken] + 1) == 0) {
                 newpar[TOTAL]++;
                 newpar[BADINPDQ] = 1;
-                if (getArgS (argv, argc, &ctoken, &par->badinpdq))
+                if (getArgS(argv, argc, &ctoken, &par->badinpdq))
                     return (status = INVALID_VALUE);
 
-            /* No match. */
-            } else
-                return (syntax_error (argv[ctoken]));
+            } else {
+                usage(argv[0]);
+                return (syntax_error(argv[ctoken]));
             }
         }
     }
+
+    if (posargs_count <= posargs_max) {
+        if (posargs_count == 0) {
+            fprintf(stderr, "Missing positional argument: input\n");
+            usage(argv[0]);
+            return (status = INVALID_VALUE);
+        }
+        if (posargs_count == 1) {
+            fprintf(stderr, "Missing positional argument: output\n");
+            usage(argv[0]);
+            return (status = INVALID_VALUE);
+        }
+    } else {
+        fprintf(stderr, "Too many positional arguments (expected %d, but received %d)\n", posargs_max, posargs_count + 1);
+        usage(argv[0]);
+        return (status = INVALID_VALUE);
+    }
+
+    *input = posargs[0];
+    strcpy(output, posargs[1]);
+
 
     return (0);
 }
@@ -251,7 +298,7 @@ static int getArgR (char **argv, int argc, int *ctoken, float *value) {
 static int syntax_error (char *msg) {
     extern int status;
 
-    printf ("Syntax error: %s\n", msg);
+    fprintf (stderr, "Syntax error: %s\n", msg);
     return (status = ERROR_RETURN);
 }
 
