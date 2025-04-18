5ededc6ebd52d009675658ce17869edc5ec0362d
Joseph Hunkeler <jhunkeler@gmail.com>
2025-04-18 14:26:29 -0400

    sprintf to snprintf
    * Caveat: history is the same size as scidata->input.
    * The history buffer should be larger, or the input buffer should be smaller.

--- pkg/stis/lib/cs11/subsci.c
+++ pkg/stis/lib/cs11/subsci.c
@@ -143,18 +143,14 @@ StisInfo11 *scidata  i: info for science data
 	    freeSingleGroup (&sci);
 
 	    if (extverWav == 1) {
-		snprintf (history, sizeof(history) - 1 - strlen(scidata->input), "Science file %s was subtracted.\n",
-			scidata->input);
+		snprintf (history, sizeof(history), "Science file %s was subtracted.\n", scidata->input);
 		addHistoryKw (wav.globalhdr, history);
 		if (hstio_err())
 		    return (HEADER_PROBLEM);
 	    }
 
-	    printf ("         %s[EXTVER=%d] was subtracted.\n",
-			scidata->input, extverSci);
-
-	    snprintf (history, sizeof(history), "%s[EXTVER=%d] was subtracted\n",
-			scidata->input, extverSci);
+	    printf ("         %s[EXTVER=%d] was subtracted.\n", scidata->input, extverSci);
+	    snprintf (history, sizeof(history), "%s[EXTVER=%d] was subtracted\n", scidata->input, extverSci);
 	    addHistoryKw (&wav.sci.hdr, history);
 	    if (hstio_err())
 		return (HEADER_PROBLEM);
