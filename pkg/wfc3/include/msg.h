71fe6868907df3bb130b574c942e20ae0298e5b1
Joseph Hunkeler <jhunkeler@gmail.com>
2025-05-06 10:01:47 -0400

    Begin WFC3 conversion
    * Remove message functions and prototypes for asn and cte

--- pkg/wfc3/include/msg.h
+++ pkg/wfc3/include/msg.h
@@ -10,14 +10,4 @@
 /* HSTIO error check function */
 void errchk ();
 
-/* The following function definitions handle the messages created
-	during operations.  These will have counterparts which
-	send output both the STDOUT and a TRL file. 
-*/
-void asnwarn (char *message);
-void asnerror (char *message);
-void ctemessage (char *message);
-void cteerror (char *message);
-void ctewarn (char *message);
-
 #endif /* INCL_MSG_H */
