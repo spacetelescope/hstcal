f33817e20c3f495e5644aa205f41c8ce27d31c0c
Joseph Hunkeler <jhunkeler@gmail.com>
2025-05-07 16:15:37 -0400

    Add rechar_s to hstcal library
    Expose symbol in str_util.h

--- lib/str_util.c
+++ lib/str_util.c
@@ -1,9 +1,29 @@
-#include <stdbool.h>
 #include <assert.h>
 #include <string.h>
 #include <fitsio.h>
 
 #include "str_util.h"
+
+/**
+ * Fills a buffer with `ch` up to `max_ch` times.
+ * The `dest_size` must be large enough to properly terminate `dest`
+ *
+ * @param ch character to repeat
+ * @param max_ch maximum number of times to repeat `ch`
+ * @param dest destination buffer to write characters (must be pre-allocated)
+ * @param dest_size size of destination buffer
+ * @return  0 on success
+ *          -1 if `dest` is `NULL`, or `dest_size` is `0`
+ */
+int repchar_s(const char ch, size_t max_ch, char * restrict dest, size_t dest_size) {
+    if (!dest || !dest_size) {
+        return -1;
+    }
+    const size_t limit = (max_ch < dest_size - 1) ? max_ch : dest_size - 1;
+    memset(dest, ch, limit);
+    dest[limit] = '\0';
+    return 0;
+}
 
 void upperCase(char * str)
 {
