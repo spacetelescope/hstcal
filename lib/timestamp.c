dff3796f070965c7dd6cd9672f08490c6e0bbb6f
Joseph Hunkeler <jhunkeler@gmail.com>
2025-05-28 10:08:39 -0400

    Rewrite TimeStamp function
    * Add comments

--- lib/timestamp.c
+++ lib/timestamp.c
@@ -8,60 +8,89 @@
 #include "trlbuf.h"
 #include "timestamp.h"
 
-/* This routine prints the current date and time into a string
-   in the format preferred by OPUS, date & time = YYYYDDDHHMMSS.
-   Example:
 
-1996212152529-I--------------- Generic Conversion Completed: N2RUSG54B ---------
+/**
+ * This routine prints the current date and time into a string
+ * in the format preferred by OPUS, date & time = YYYYDDDHHMMSS.
+ *
+ * Example:
+ * @code{.c}
+ * #include "timestamp.h"
+ * int main(int argc, char *argv[]) {
+ *     InitTrlBuf();
+ *     TimeStamp("Generic Conversion Complete", "N2RUSG54B");
+ *     CloseTrlBuf(&trlbuf);
+ * }
+ * @endcode
+ *
+ * Output:
+ * @code
+ * 1996212152529-I--------------- Generic Conversion Completed: N2RUSG54B ---------
+ * @endcode
+ *
+ * @param message string to append to date & time info
+ * @param rootname root name to include in printed string
 */
 
 void TimeStamp(char *message, const char *rootname) {
-    /* arguments:
-    char *message   i: string to append to date & time info
-    char *rootname  i: root name to include in printed string
-    */
+    // Separates time string and the message[: rootname]
+    const char *message_sep = "-I---------------";
 
-    char *timestring = NULL; /* string to be printed */
-    char *uc_rootname = NULL; /* rootname converted to upper case */
-    size_t lenrootname = 0; /* length of rootname string */
-    time_t *tp = NULL;
-    time_t now;
-
-    if ((timestring = malloc(SZ_TIMESTRING * sizeof(char))) == NULL) {
+    // Configure timestamp
+    // Y - Year (YYYY)
+    // j - Day of the year (001-360)
+    // H - Hour (00-23)
+    // M - Minute (0-59)
+    // S - seconds (0-60)
+    const char *time_fmt = "%Y%j%H%M%S";
+    char *time_str = calloc(SZ_TIMESTRING, sizeof(*time_str));
+    if (!time_str) {
         trlerror("(TimeStamp) Out of memory.");
         return;
     }
 
+    // Populate time string
+    const time_t tp = time(NULL);
+    const struct tm *now = localtime(&tp);
+    strftime(time_str, SZ_TIMESTRING, time_fmt, now);
+
+    // Optional root name
+    char *uc_rootname = NULL;
+    // Length of root name
+    size_t uc_rootname_len = 0;
+
     if (rootname) {
-        lenrootname = strlen(rootname);
-        if (lenrootname > 0) {
-            uc_rootname = malloc((lenrootname + 1) * sizeof(char));
-            if (uc_rootname == NULL) {
-                trlerror("(TimeStamp) Out of memory.");
-                free(timestring);
-                return;
-            }
-            strcpy(uc_rootname, rootname);
-            for (size_t i = 0; i < strlen(rootname); i++) {
-                if (islower(uc_rootname[i])) {
-                    uc_rootname[i] = (char) toupper(uc_rootname[i]);
-                }
-            }
+        uc_rootname = strdup(rootname);
+        if (uc_rootname == NULL) {
+            trlerror("(TimeStamp) Out of memory.");
+            return;
         }
+        // Convert root name to upper case
+        upperCase(uc_rootname);
+        uc_rootname_len = strlen(uc_rootname);
     }
 
-    tp = NULL;
-    now = time(tp);
-    strftime(timestring, SZ_TIMESTRING, "%Y%j%H%M%S", localtime(&now));
-    snprintf(timestring + strlen(timestring),
-        SZ_TIMESTRING - strlen(timestring),
-        "-I--------------- %s%s%s ", message, lenrootname > 0 ? ": " : "", lenrootname > 0 ? uc_rootname : "");
+    // Populate the output buffer
+    const size_t output_max_len = SZ_EIGHTY;
+    char *output = calloc(output_max_len + 1, sizeof(*output));
+    snprintf(output, output_max_len, "%s%s %s%s%s ",
+        time_str,
+        message_sep,
+        message,
+        uc_rootname_len ? ": " : "",
+        uc_rootname_len ? uc_rootname: "");
 
-    /* Fill out to 80 bytes with dashes. */
-    repchar_s('-', SZ_EIGHTY, timestring + strlen(timestring), SZ_TIMESTRING - strlen(timestring));
+    // Pad remaining bytes in the output buffer with dashes
+    repchar_s('-',
+        output_max_len - strlen(output),
+        &output[strlen(output)],
+        output_max_len);
 
-    trlmessage("%s", timestring);
+    // Display output
+    trlmessage("%s", output);
 
+    // Clean up
     free(uc_rootname);
-    free(timestring);
+    free(time_str);
+    free(output);
 }
