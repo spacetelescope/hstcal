df4fb471d4117733365253d68873c1e7f60e1b6d
Joseph Hunkeler <jhunkeler@gmail.com>
2025-07-02 12:00:42 -0400

    Rename VERSION, BRANCH, and COMMIT
    * Now HSTCAL_VERSION, HSTCAL_VERSION_BRANCH, and HSTCAL_VERSION_COMMIT

--- lib/hstcalversion.c
+++ lib/hstcalversion.c
@@ -13,11 +13,14 @@ char * sprintfGitInfo(char ** buffer)
         assert(0); // Incorrect usage - NULL ptr must be passed in.
 
     const char * format = "git tag: %s\ngit branch: %s\nHEAD @: %s";
-    size_t length = strlen(format) + strlen(VERSION) + strlen(BRANCH) + strlen(COMMIT); // NOTE: this doesn't require an explicit +1 for '\0' as it is larger than needed due to '%s' & '\n'.
+    size_t length = strlen(format)
+        + strlen(HSTCAL_VERSION)
+        + strlen(HSTCAL_VERSION_BRANCH)
+        + strlen(HSTCAL_VERSION_COMMIT); // NOTE: this doesn't require an explicit +1 for '\0' as it is larger than needed due to '%s' & '\n'.
     *buffer = malloc((length + 1) * sizeof(char));
     if (!*buffer)
         return NULL;
-    snprintf(*buffer, length, format, VERSION, BRANCH, COMMIT);
+    snprintf(*buffer, length, format, HSTCAL_VERSION, HSTCAL_VERSION_BRANCH, HSTCAL_VERSION_COMMIT);
     return *buffer;
 }
 
