11fbda7bfae838b8791f1c6537bb4ff62c2267ab
Robert Jedrzejewski <rij@stsci.edu>
2026-02-02 10:08:10 -0500

    Bump version

--- pkg/stis/include/stisversion.h
+++ pkg/stis/include/stisversion.h
@@ -3,6 +3,6 @@
 
 /* This string is written to the output primary header as CAL_VER. */
 
-# define STIS_CAL_VER "3.4.2 (19-Jan-2018)"
+# define STIS_CAL_VER "3.5.0 (02-Feb-2026)"
 
 #endif /* INCL_STISVERSION_H */
