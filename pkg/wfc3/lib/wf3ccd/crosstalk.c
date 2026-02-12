3926f98d4fda8540162715ff173d8a658ba6f1b7
Pey Lian Lim <2090236+pllim@users.noreply.github.com>
2026-02-11 22:58:43 -0500

    Bug fix rabbit hole

--- pkg/wfc3/lib/wf3ccd/crosstalk.c
+++ pkg/wfc3/lib/wf3ccd/crosstalk.c
@@ -4,9 +4,6 @@
 #include "wf3.h"
 #include "wf3info.h"
 
-static void dn_to_e (SingleGroup *, int, int, int, float *);
-static void e_to_dn (SingleGroup *, int, int, int, float *);
-
 
 /* remove amplifier cross-talk */
 int cross_talk_corr(WF3Info *wf3, SingleGroup *x) {
@@ -14,18 +11,14 @@ int cross_talk_corr(WF3Info *wf3, SingleGroup *x) {
     const int arr_rows = x->sci.data.ny;
     const int arr_cols = x->sci.data.nx;
     int i, j, cur_amp;  /* iteration variables */
-    float corr_fac, cur_err, *y;
+    float *y;
+    double corr_fac, cur_err;
 
     /* Correction coefficients (ABCD) from WFC3 ISR 2012-02 */
-    const float intercept[NAMPS] = {0.0180206, 0.15501201,-0.038376406, 0.19124641};
-    const float slope[NAMPS] = {-0.060494304, -0.20746221, -0.079701178, -0.23177171};
+    const double intercept[NAMPS] = {0.0180206, 0.15501201,-0.038376406, 0.19124641};
+    const double slope[NAMPS] = {-6.0494304e-5, -2.0746221e-4, -7.9701178e-5, -2.3177171e-4};
 
-    /* Convert to electrons */
-    for (i = 0; i < arr_rows; i++) {
-        dn_to_e(x, i, wf3->ampx, wf3->ampy, wf3->atodgain);
-    }
-
-    /* Crosstalk correction */
+    /* Crosstalk correction in electrons but then converted back to DN */
     for (i = 0; i < arr_rows; i++) {
         /* Copy out original row for corr_fac calculation. */
         if ((y = calloc(arr_cols, sizeof(float))) == NULL) {
@@ -36,7 +29,7 @@ int cross_talk_corr(WF3Info *wf3, SingleGroup *x) {
         }
 
         for (j = 0; j < arr_cols; j++) {
-            if (i < wf3->ampy) {
+            if (x->group_num == 1) {
                 if (j < wf3->ampx) {
                     cur_amp = AMP_C;
                 } else {
@@ -49,111 +42,14 @@ int cross_talk_corr(WF3Info *wf3, SingleGroup *x) {
                     cur_amp = AMP_B;
                 }
             }
-            corr_fac = intercept[cur_amp] + y[arr_cols - j - 1] * slope[cur_amp];
-            Pix(x->sci.data, j, i) = y[j] - corr_fac;
+            corr_fac = intercept[cur_amp] + y[arr_cols - j - 1] * wf3->atodgain[cur_amp] * slope[cur_amp];
+            Pix(x->sci.data, j, i) = (y[j] * wf3->atodgain[cur_amp] - corr_fac) / wf3->atodgain[cur_amp];
 
             /* Propagate error; assume ERR of correction is sqrt(corr_fac) */
-            cur_err = Pix(x->err.data, j, i);
-            Pix(x->err.data, j, i) = (float) sqrt(cur_err * cur_err + fabs(corr_fac));
+            cur_err = Pix(x->err.data, j, i) * wf3->atodgain[cur_amp];
+            Pix(x->err.data, j, i) = sqrt(cur_err * cur_err + fabs(corr_fac)) / wf3->atodgain[cur_amp];
         }
-    }
-
-    /* Convert back to DN */
-    for (i = 0; i < arr_rows; i++) {
-        e_to_dn(x, i, wf3->ampx, wf3->ampy, wf3->atodgain);
     }
 
     return status;
-}
-
-
-static void dn_to_e (SingleGroup *a, int line, int ampx, int ampy, float *gain) {
-
-/* arguments:
-SingleGroupLine *a	io: input data; output product
-int line		i: line number of input line
-int ampx, ampy		i: amp parameters
-float *gain		i: atogain parameter
-
-Adapted from multgn1d for fullframe only.
-*/
-	int i;
-	int dimx=a->sci.data.nx;
-
-    /* Since both the science and error arrays operate on the
-       same line of data, we will combine them into one loop over
-       X values.
-    */
-	if (line < ampy) {
-        /* This line has 2-AMP readout */
-        /* Apply gain for first amp to first part of line */
-        for (i = 0;  i < ampx;  i++) {
-            Pix(a->sci.data, i, line) = gain[AMP_C] * Pix(a->sci.data, i, line);
-	        Pix(a->err.data, i, line) = gain[AMP_C] * Pix(a->err.data, i, line);
-        }
-        /* Apply gain for second amp over remainder of line */
-        for (i = ampx;  i < dimx;  i++) {
-            Pix(a->sci.data, i, line) = gain[AMP_D] * Pix(a->sci.data, i, line);
-	        Pix(a->err.data, i, line) = gain[AMP_D] * Pix(a->err.data, i, line);
-        }
-
-    } else {
-        /* This line has 2-AMP readout */
-        /* Apply gain for first amp to first part of line */
-        for (i = 0;  i < ampx;  i++) {
-            Pix(a->sci.data, i, line) = gain[AMP_A] * Pix(a->sci.data, i, line);
-	        Pix(a->err.data, i, line) = gain[AMP_A] * Pix(a->err.data, i, line);
-        }
-        /* Apply gain for second amp over remainder of line */
-        for (i = ampx;  i < dimx;  i++) {
-            Pix(a->sci.data, i, line) = gain[AMP_B] * Pix(a->sci.data, i, line);
-            Pix(a->err.data, i, line) = gain[AMP_B] * Pix(a->err.data, i, line);
-        }
-    }
-}
-
-
-static void e_to_dn (SingleGroup *a, int line, int ampx, int ampy, float *gain) {
-
-    /* arguments:
-    SingleGroupLine *a	io: input data; output product
-    int line		i: line number of input line
-    int ampx, ampy		i: amp parameters
-    float *gain		i: atogain parameter
-
-    Adapted from multgn1d for fullframe only.
-    */
-    int i;
-    int dimx=a->sci.data.nx;
-
-    /* Since both the science and error arrays operate on the
-       same line of data, we will combine them into one loop over
-       X values.
-    */
-    if (line < ampy) {
-        /* This line has 2-AMP readout */
-        /* Apply gain for first amp to first part of line */
-        for (i = 0;  i < ampx;  i++) {
-            Pix(a->sci.data, i, line) = Pix(a->sci.data, i, line) / gain[AMP_C];
-            Pix(a->err.data, i, line) = Pix(a->err.data, i, line) / gain[AMP_C];
-        }
-        /* Apply gain for second amp over remainder of line */
-        for (i = ampx;  i < dimx;  i++) {
-            Pix(a->sci.data, i, line) = Pix(a->sci.data, i, line) / gain[AMP_D];
-            Pix(a->err.data, i, line) = Pix(a->err.data, i, line) / gain[AMP_D];
-        }
-
-    } else {
-        /* This line has 2-AMP readout */
-        /* Apply gain for first amp to first part of line */
-        for (i = 0;  i < ampx;  i++) {
-            Pix(a->sci.data, i, line) = Pix(a->sci.data, i, line) / gain[AMP_A];
-            Pix(a->err.data, i, line) = Pix(a->err.data, i, line) / gain[AMP_A];
-        }
-        /* Apply gain for second amp over remainder of line */
-        for (i = ampx;  i < dimx;  i++) {
-            Pix(a->sci.data, i, line) = Pix(a->sci.data, i, line) / gain[AMP_B];
-            Pix(a->err.data, i, line) = Pix(a->err.data, i, line) / gain[AMP_B];
-        }
-    }
 }
