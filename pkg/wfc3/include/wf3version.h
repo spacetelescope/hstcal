dad773fb7130fcc1a199c201bf205b67df63c050
P. L. Lim <2090236+pllim@users.noreply.github.com>
2026-07-29 21:31:27 -0400

    TMP: Debugging for Norman but this should not be merged

--- pkg/wfc3/include/wf3version.h
+++ pkg/wfc3/include/wf3version.h
@@ -4,7 +4,7 @@
 /* This string is written to the output primary header as CAL_VER. */
 
 
-# define WF3_CAL_VER "3.8.0 (Feb-07-2026)"
-# define WF3_CAL_VER_NUM "3.8.0"
+# define WF3_CAL_VER "3.8.1 (Jul-29-2026)"
+# define WF3_CAL_VER_NUM "3.8.1"
 
 #endif /* INCL_WF3VERSION_H */
