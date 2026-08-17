a43435df86f98c86511b2508df187a94fe4dafda
P. L. Lim <2090236+pllim@users.noreply.github.com>
2026-08-17 16:33:56 -0400

    Move y calloc out of loop

--- pkg/wfc3/lib/wf3ccd/crosstalk.c
+++ pkg/wfc3/lib/wf3ccd/crosstalk.c
@@ -22,12 +22,13 @@ int cross_talk_corr(WF3Info *wf3, SingleGroup *x) {
     const double intercept[NAMPS] = {0, 0, 0, 0};
     const double slope[NAMPS] = {-6.0494304e-5, -2.0746221e-4, -7.9701178e-5, -2.3177171e-4};
 
+    if ((y = calloc(arr_cols, sizeof(float))) == NULL) {
+        return (status = OUT_OF_MEMORY);
+    }
+
     /* Crosstalk correction in electrons but then converted back to DN */
     for (i = 0; i < arr_rows; i++) {
         /* Copy out original row for corr_fac calculation. */
-        if ((y = calloc(arr_cols, sizeof(float))) == NULL) {
-            return (status = OUT_OF_MEMORY);
-        }
         for (j = 0; j < arr_cols; j++) {
             y[j] = Pix(x->sci.data, j, i);
         }
@@ -68,5 +69,6 @@ int cross_talk_corr(WF3Info *wf3, SingleGroup *x) {
         }
     }
 
+    free(y);
     return status;
 }
