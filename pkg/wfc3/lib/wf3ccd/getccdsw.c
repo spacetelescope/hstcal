f4fb7c82db08dd62f35d7b1c72d0f8d7c19c6731
P. L. Lim <2090236+pllim@users.noreply.github.com>
2026-09-11 18:00:10 -0400

    Indentation of getccdsw.c

--- pkg/wfc3/lib/wf3ccd/getccdsw.c
+++ pkg/wfc3/lib/wf3ccd/getccdsw.c
@@ -1,7 +1,6 @@
-# include <stdio.h>
-# include "hstio.h"
-# include "wf3.h"
-# include "wf3info.h"
+#include "hstio.h"
+#include "wf3.h"
+#include "wf3info.h"
 
 static int GetSw (Hdr *, char *, int *);
 
@@ -15,23 +14,22 @@ the header exists and is "PERFORM".
 
 int GetccdSw (WF3Info *wf3, Hdr *phdr) {
 
+    extern int status;
 
-	extern int status;
-
-	if (GetSw (phdr, "ATODCORR", &wf3->atodcorr))
-	    return (status);
-	if (GetSw (phdr, "BIASCORR", &wf3->biascorr))
-	    return (status);
-	if (GetSw (phdr, "BLEVCORR", &wf3->blevcorr))
-	    return (status);
-	if (GetSw (phdr, "DQICORR",  &wf3->dqicorr))
-	    return (status);
-	if (GetSw (phdr, "FLSHCORR", &wf3->flashcorr))
-	    return (status);
+    if (GetSw (phdr, "ATODCORR", &wf3->atodcorr))
+        return (status);
+    if (GetSw (phdr, "BIASCORR", &wf3->biascorr))
+        return (status);
+    if (GetSw (phdr, "BLEVCORR", &wf3->blevcorr))
+        return (status);
+    if (GetSw (phdr, "DQICORR",  &wf3->dqicorr))
+        return (status);
+    if (GetSw (phdr, "FLSHCORR", &wf3->flashcorr))
+        return (status);
     if (GetSw (phdr, "PCTECORR", &wf3->pctecorr))
         return(status);
 
-	return (status);
+    return (status);
 }
 
 /* This routine just calls GetSwitch to get the value of the switch,
@@ -46,15 +44,15 @@ char *calswitch  i: name of keyword (e.g. FLATCORR)
 int *flag        o: value (0 or 1) of calibration switch
 */
 
-	extern int status;
+    extern int status;
 
-	int GetSwitch (Hdr *, char *, int *);
+    int GetSwitch (Hdr *, char *, int *);
 
-	if (GetSwitch (phdr, calswitch, flag))
-	    return (status);
+    if (GetSwitch (phdr, calswitch, flag))
+        return (status);
 
-	if (*flag != PERFORM)
-	    *flag = OMIT;
+    if (*flag != PERFORM)
+        *flag = OMIT;
 
-	return (status);
+    return (status);
 }
