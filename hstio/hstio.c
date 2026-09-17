2abe79d88188959ab643f003897595b3bf576bdb
Joseph Hunkeler <jhunkeler@gmail.com>
2026-09-17 10:21:08 -0400

    Replace atoi with strtol

--- hstio/hstio.c
+++ hstio/hstio.c
@@ -282,7 +282,7 @@ int findTotalNumberOfHDUSets(const char * fileName, const char * setContainsExtN
             }
         } else {
             // (python) nimsets = len(set([hdu.ver for hdu in hduList]))
-            int extVer = atoi(keyValue);
+            const int extVer = (int)strtol(keyValue, NULL, 10);
             bool alreadyCounted = false;
             // Ugly, but list size should be small so who cares
             {
